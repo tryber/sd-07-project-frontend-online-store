ce15f270b1df7f3b6e8891283ba21f22c87578b2
Brune Miguel <bstfreire@gmail.com>
2020-11-24 18:56:24 -0300

    Incluindo componente requisito 2

---
+++ src/ProductList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ProductList extends Component {
+  render() {
+    return (
+      <div>
+        <input />
+        <h1 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h1>
+      </div>
+    );
+  }  
+}
+
+export default ProductList;
