28828fdae341f0ab0f8efa3daf1203f5a4c2f45b
Renato Silva <renatosdev@gmail.com>
2020-11-24 18:49:24 -0300

    Cria componente de busca - cc

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
       <header>
         <input type="text" />
-        <h2 data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+        <h2 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h2>
       </header>
     </div>
   );
