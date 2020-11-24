1129681e23c7f41bb360f3e95459e4e6b9941e3c
dennerpl <denner.souzapl@gmail.com>
2020-11-24 18:08:02 -0300

    Página inicial e carrinho de compras criadas

---
+++ src/pages/SearchPage.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class SearchPage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            productsToRender: []
+        }
+    }
+    
+    render() {
+      return(<div>
+        <label>
+        <button>Botão de pesquisa</button>
+        <input type="text"></input>
+        </label>
+        <Link to="/shoppingcart"><button>Carrinho de compras</button></Link>
+        <ul data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria</ul>
+        </div>)
+    }
+  }
+  
+  export default SearchPage;
