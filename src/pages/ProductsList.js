d8dd9e7bb88ebc3a70fd7b5f65b34b072216ec3a
dennerpl <denner.souzapl@gmail.com>
2020-11-24 19:12:35 -0300

    componentizando página inicial

--- src/pages/ProductsList.js
+++ src/pages/ProductsList.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import SearchBar from '../components/SearchBar'
+import FilteredProductsList from '../components/FilteredProductsList';
 
 class ProductsList extends Component {
-    constructor() {
-        super();
-        this.state = {
-            productsToRender: []
-        }
-    }
-    
-    render() {
-      return(<div>
-        <label>
-        <button>Botão de pesquisa</button>
-        <input type="text"></input>
-        </label>
-        <Link to="/shoppingCart"><button>Carrinho de compras</button></Link>
-        <ul data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria</ul>
-        </div>)
+  constructor() {
+    super();
+    this.state = {
+      productsToRender: []
     }
   }
+
+  render() {
+    return (
+    <div>
+      <div>
+        <SearchBar />
+        <FilteredProductsList />
+      </div>
+    </div>
+    )
+  }
+}
   
   export default ProductsList;
