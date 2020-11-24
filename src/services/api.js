022fc0b5d5a1583bba605faa0f1fc7a056c74dff
Rogerio-feitor <roger.feitor@gmail.com>
2020-11-24 20:05:04 -0300

    Inserção do arquivo api.js - requisiçoes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,26 @@
 export async function getCategories() {
-  // Implemente aqui
+  const endpoints = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(endpoints);
+  const object = await response.json();
+  return object;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+function generateEndpoint(...params) {
+  let endpoints;
+  if (params.length === null) {
+    endpoints = 'https://api.mercadolibre.com/sites/MLB/categories';
+  } else if (params.length === 1 && params[0].toString().startsWith('MLB')) {
+    endpoints = `https://api.mercadolibre.com/sites/MLB/search?category=${params[0]}`;
+  } else if (params.length === 1 && params[0]) {
+    endpoints = `https://api.mercadolibre.com/sites/MLB/search?q=${params[0]}`;
+  } else if (params[0].toString().startsWith('MLB') && params[1].toString()) {
+    endpoints = `https://api.mercadolibre.com/sites/MLB/search?category=${params[0]}&q=${params[1]}`;
+  }
+  return endpoints;
+}
+
+export async function getProductsFromCategoryAndQuery(...params) {
+  const response = await fetch(generateEndpoint(...params));
+  const object = await response.json();
+  return object;
 }
