7a329dbe3047f008d05b50a46f13efe8414f37c6
moises <moisaant@gmail.com>
2020-11-24 17:20:51 -0300

    requisição feita com sucesso

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
-  // Implemente aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const fetchResult = await fetch(url);
+  const expectedArray = await fetchResult.json();
+  return expectedArray;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const fetchResult = await fetch(url);
+  const expectedObject = await fetchResult.json();
+  return expectedObject;
 }
