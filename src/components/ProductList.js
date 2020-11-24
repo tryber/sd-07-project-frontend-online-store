29b615a69442ff23b562b3de6a08fc86e0f371c8
PedroMarqDev <PedroMarqDev@gmail.com>
2020-11-24 19:01:15 -0300

    Adicionado um botao e a mensagem que o carrinho esta vazio

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -3,9 +3,15 @@ import React, { Component } from 'react';
 class ProductList extends Component {
   render() {
     return (
-      <h1 data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </h1>
+      <div>
+        <h1 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h1>
+        <button type="button" data-testid="shopping-cart-button">Carrinho</button>
+        <p data-testid="shopping-cart-empty-message">
+          Seu carrinho está vazio
+        </p>
+      </div>
     );
   }
 }
