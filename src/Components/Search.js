e7bad3dd578d14e7dbcdcd177d6971c4017c5a57
Strongreen <strongreen@live.com>
2020-11-24 19:38:55 -0300

    Implementaçao do card Product, card List e alteraçoes no Search, inclusdao do input

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,13 +1,51 @@
 import React from 'react';
+import '../App.css';
+import * as api from '../services/api';
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      product: {},
+      categoryId: 0,
+      loading: false,
+    };
+    this.getProduct = this.getProduct.bind(this);
+  }
+
+  componentDidMount() {
+	this.getProduct()
+  }
+
+  getProduct() {
+    this.setState({ loading: true }, async () => {
+      const getingProduct = await api.getProductsFromCategoryAndQuery(
+        this.state.categoryId,
+        this.state.term
+      );
+      this.setState({ loading: false, product: getingProduct });
+    });
+  }
+
   render() {
-	  return (
-	    <div>
-				<p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <input
+          data-testid="query-input"
+          className="input-search"
+          type="text"
+          placeholder="Digite algum termo de pesquisa aqui"
+        />
+        <button className="button-search" data-testid="query-button">
+          Procurar
+        </button>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
+    );
+  }
 }
 
 export default Search;
