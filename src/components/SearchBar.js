6bd0742c8f2765eea19f4c8cac0ea360e58c324c
murilopereira006 <muriloedu006@gmail.com>
2020-11-24 19:48:54 -0300

    mains.yml versao corrigida. Favor atualizar sua branch

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,13 @@
 import React from 'react'
 
 class SearchBar extends React.Component {
-  
     render() {
-
         return (
             <div>
                 <input type="text"></input>
                 <div data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</div>
             </div>
         )
-
     }
 }
 
