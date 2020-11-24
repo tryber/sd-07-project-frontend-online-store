a2737ef68d31a60d68b0921162dd1a476e7d333b
brendalima <brenda.c.lima@poli.ufrj.br>
2020-11-24 18:49:41 -0300

    creating shopping cart button

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header>
         <input type="text" data-testid="home-initial-message" />
         <h2>Digite algum termo de pesquisa ou escolha uma categoria.</h2>
+        <button data-testid="shopping-cart-button">Carrinho de compras</button>
       </header>
     </div>
   );
