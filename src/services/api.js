06b1dc0e40062df598976292d94515ff6df3c4e8
Eduardo Pizorno <pizorno@gmail.com>
2020-11-24 12:35:00 -0300

    Api refatorada

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,11 @@
 export async function getCategories() {
   const Request = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  const Json = await Request.json();
-  return Json;
+  const Result = await Request.json();
+  return Result;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const Request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
-  const Json = await Request.json();
-  return Json;
+  const Result = await Request.json();
+  return Result;
 }
