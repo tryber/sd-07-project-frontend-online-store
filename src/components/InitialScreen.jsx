032ae5c60fff3524f40624516e56aa7b8bdf7765
vini-lima <vini20.henrique@gmail.com>
2020-11-24 16:45:36 -0300

    SideBar

--- src/components/InitialScreen.jsx
+++ src/components/InitialScreen.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
+import { SideBar } from './index';
+import './InitialScreen.css';
+
+import * as api from '../services/api';
 
 class InitialScreen extends React.Component {
   constructor() {
     super();
     this.state = {
+      categories: [],
       message: 'Digite algum termo de pesquisa ou escolha uma categoria.',
     };
+  }
+
+  async componentDidMount() {
+    await this.fetchAPI();
+  }
+
+  async fetchAPI() {
+    const { getCategories } = api;
+
+    this.setState({
+      categories: await getCategories(),
+    });
   }
 
   render() {
     const { message } = this.state;
     return (
-      <div>
-        <input type="text" />
-        <h1 data-testid="home-initial-message">{message}</h1>
+      <div className="container">
+        <div className="side-bar">
+          <SideBar />
+        </div>
+        <div className="container-initial-screen">
+          <input type="text" />
+          <h1 data-testid="home-initial-message">{message}</h1>
+        </div>
       </div>
     );
   }
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SideBar extends Component {
+  render() {
+    return (
+      <span>SideBar</span>
+    );
+  }
+}
+
+export default SideBar;
