aeabef8dec7dd80fd6920f9726a47da4a12d29db
Rogerio-feitor <roger.feitor@gmail.com>
2020-11-24 20:56:12 -0300

    Corrigindo o App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Lojinha do Clube da Luta</h1>
       </header>
     </div>
   );
