4b28b52cd8730911c06749f60d5c5005b7695707
Samanta Below <samanta.below@gmail.com>
2020-11-24 18:00:01 -0300

    Correção dos erros

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import * as api from './services/api'
+import * as api from './services/api';
 import './App.css';
 
-api.getCategories().then(categories => { console.log(categories) })
-api.getProductsFromCategoryAndQuery().then(categories => { console.log(categories) })
+api.getCategories().then((categories) => { console.log(categories) });
+api.getProductsFromCategoryAndQuery().then((categories) => { console.log(categories) });
 
 function App() {
   return (
-    <div className="App">
+    // <div className="App">
 
-    </div>
+    // </div>
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,18 @@
 export async function getCategories() {
-  return new Promise((resolve, reject) => {
-    const categories = "https://api.mercadolibre.com/sites/MLB/categories";
+  return new Promise((resolve) => {
+    const categories = 'https://api.mercadolibre.com/sites/MLB/categories';
     const fetchCategories = fetch(categories)
       .then((data) => data.json());
     resolve(fetchCategories);
-  })
+  });
 }
 
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  return new Promise((resolve, reject) => {
-    const categories = "https://api.mercadolibre.com/sites/MLB/search?q=$QUERY";
+  return new Promise((resolve) => {
+    const categories = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
     const fetchByItem = fetch(categories)
       .then((data) => data.json());
     resolve(fetchByItem);
-  })
+  });
 }
