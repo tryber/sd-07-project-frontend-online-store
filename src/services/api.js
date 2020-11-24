94e143a55b5a81ea47a4381d95e1e42822891688
Pedro Marques <pedromarques1011@gmail.com>
2020-11-24 12:02:51 -0300

    try/catch req1

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,21 @@
 export async function getCategories() {
-  const req = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  const json = await req.json();
-
-  return json;
+  try {
+    const req = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+    const json = await req.json();
+    return json;
+  } catch (error) {
+    console.log(`(From async/await getCategories) Error: ${error}`);
+  }
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const req = await fetch(
-    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`,
-  );
-  const json = await req.json();
-
-  return json;
+  try {
+    const req = await fetch(
+      `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`,
+    );
+    const json = await req.json();
+    return json;
+  } catch (error) {
+    console.log(`(From async/await getProductsFromCategoryAndQuery) Error: ${error}`);
+  }
 }
