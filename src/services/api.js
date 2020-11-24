0688a9cb869b2487a001a08b6abdf182c4181992
luc-zago <luc.zago@hotmail.com>
2020-11-24 09:24:59 -0300

    Implementadas funções 'getCategories' e 'getProductsFromCategoryAndQuer'

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,25 @@
+// faz a chamada da API
+const makeRequest = (url) => fetch(url).then((response) => response.json());
+
 export async function getCategories() {
   // Implemente aqui
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const myReturn = makeRequest(url);
+  await myReturn;
+  return myReturn;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  if (query === undefined) {
+    const url = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}`;
+    const myReturn = makeRequest(url);
+    await myReturn;
+    return myReturn;
+  }
+
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=$${categoryId}&q=$${query}`;
+  const myReturn = makeRequest(url);
+  await myReturn;
+  return myReturn;
 }
