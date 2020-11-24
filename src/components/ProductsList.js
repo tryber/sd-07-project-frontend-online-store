90b42c261ec6c7bcfba941c2625dc9fed3377b40
Felipe Nascimento <flpnascto@gmail.com>
2020-11-24 18:10:47 -0300

    Corringo texto do ProductsList

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -3,7 +3,7 @@ import React from 'react';
 class ProductsList extends React.Component {
   render() {
     return (
-      <div data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria</div>
+      <div data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</div>
     );
   }
 }
