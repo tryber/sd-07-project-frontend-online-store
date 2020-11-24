a502116fec836363061443c57e7fdf79563b83d7
Vitor Rodrigues <vitor_rc@outlook.com>
2020-11-24 18:07:26 -0300

    Ajustes Lint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Products from './pages/Products';
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Products} /> 
+          <Route exact path="/" component={ Products } />
         </Switch>
       </BrowserRouter>
     </div>
