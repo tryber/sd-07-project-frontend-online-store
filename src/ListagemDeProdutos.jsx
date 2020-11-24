832e802cc50d507b5186b67d893c47781d7594bb
vitorAmorims <vtamorims@gmail.com>
2020-11-24 11:51:39 -0300

    Requisito 2

---
+++ src/ListagemDeProdutos.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class ListagemDeProdutos extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <h1 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h1>
+      </div>
+    );
+  }
+}
+
+export default ListagemDeProdutos;
