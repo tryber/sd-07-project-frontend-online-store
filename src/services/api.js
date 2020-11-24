e596ea6c21a3ae88e4ade1a36a6ee8aabdd900b1
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-11-24 17:26:40 -0300

    implementation getcategories function

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 export async function getCategories() {
-  // Implemente aqui
+  const endPoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const request = await fetch (endPoint);
+  const object = await request.json();
+  return object;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
 }
+
+console.log(getCategories())
