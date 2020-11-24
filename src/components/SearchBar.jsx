7f8a2b91f43a6feaa70caf618d50c83b4512b8e3
Henrique Coppola <hvenses@gmail.com>
2020-11-24 18:52:35 -0300

    Shopping Cart

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import cart from './img/shopping.png';
 class SearchBar extends React.Component {
   render() {
     return (
       <div>
-        <label>
-          <input type="text" />
-        </label>
+        <div>
+          <label>
+            <input type="text" />
+          </label>
+          <Link to="/shoppingcart">
+            <img src={cart} alt="Shopping Cart"/>
+          </Link>
+        </div>
         <h3 data-testid="home-initial-message" >Digite algum termo de pesquisa ou escolha uma categoria.</h3>
       </div>
     );
