47f3f364f765b95cdebfc043a70aa14da34b44d0
Berilo Wanderley <wberilo@gmail.com>
2020-11-24 14:50:48 -0300

    req complete

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import * as api from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    // exemplos de utilização da API
+    api.getCategories().then((categories) => console.log(categories));
+    api
+      .getProductsFromCategoryAndQuery('MLB5672', 'Pneu')
+      .then((result) => console.log(result));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Title</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
