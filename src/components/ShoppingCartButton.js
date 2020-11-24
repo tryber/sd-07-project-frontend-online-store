66855a3ab819111df3eff929e10a29dc1cab16ba
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-11-24 19:14:35 -0300

    criando componente ShoppingCartButton.

---
+++ src/components/ShoppingCartButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class ShoppingCartButton extends Component {
+  render() {
+    return (
+      <Link data-testid="shopping-cart-button" to="/shopping-cart">Carrinho de compras</Link>
+    );
+  }
+}
+
+export default ShoppingCartButton;
