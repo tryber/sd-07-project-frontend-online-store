2b03ae7559c892dba1c14113e3345be83500b638
vitorAmorims <vtamorims@gmail.com>
2020-11-24 19:19:24 -0300

    requisito5

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
-import * as API from '../services/api';
 
 class Product extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        }
-    }
-    componentDidMount() {
-        
-    }
-
-    async requestProducts() {
-        const ProductList = await API.getCategories();
-        this.setState({ categories: categoriesList })
-    }
-    render() {
-        return ()
-    }
+  render() {
+    const { title, price, thumbnail } = this.props;
+    return (
+      <div data-testid="product">
+        <span>{title}</span>
+        <img src={thumbnail} alt="product image" />
+        <span>{price}</span>
+      </div>
+    );
+  }
 }
 
 export default Product;
--- src/pages/ProductsList.jsx
+++ src/pages/ProductsList.jsx
@@ -3,17 +3,20 @@ import { Link } from "react-router-dom";
 import * as API from "../services/api";
 import CategoriesList from "../components/CategoriesList";
 import Logo from "../shoppingCartImage.png";
+import Product from "./Product";
 
 class ProductsList extends Component {
   constructor() {
     super();
+    this.showProducts = this.showProducts.bind(this);
+    this.showMessage = this.showMessage.bind(this);
     this.requestProductsFromCategoryAndQuery = this.requestProductsFromCategoryAndQuery.bind(
       this
     );
     this.state = {
       categories: undefined,
       products: [],
-      search: '',
+      search: "",
     };
   }
 
@@ -25,13 +28,29 @@ class ProductsList extends Component {
     const categoriesList = await API.getCategories();
     this.setState({ categories: categoriesList });
   }
-  async requestProductsFromCategoryAndQuery() {
-    const ListProducts = await API.getProductsFromCategoryAndQuery(this.state.search);
+  async searchQueryProducts() {
+    const ListProducts = await API.getQuery(); (
+      this.state.search
+    );
+    if (ListProducts === '') return (<span>Nenhum produto foi encontrado</span>);
     this.setState({ products: ListProducts });
   }
-
+  showProducts(params) {
+        return (
+          params.map((element) => {
+              <Product key={element.id} title={element.title} price={element.price} thumbnail={element.thumbnail}/>
+          })
+        )
+  }
+  showMessage() {
+    return (
+      <h1 data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h1>
+    );
+  }
   render() {
-    const { categories } = this.state;
+    const { categories, products } = this.state;
 
     return (
       <div>
@@ -41,11 +60,10 @@ class ProductsList extends Component {
         <div>
           <input
             type="text"
+            data-testid="query-input"
             onChange={this.requestProductsFromCategoryAndQuery}
           />
-          <h1 data-testid="home-initial-message">
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </h1>
+          {this.state.products === [] ? showMessage() : showProducts()}
           <Link data-testid="shopping-cart-button" to="/PageCard">
             <img src={Logo} alt="shoppingCart" />
           </Link>
