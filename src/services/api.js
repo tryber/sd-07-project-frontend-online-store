5bf67390ecc2577cb17c4a5c789298341a343ad1
Paulo Henrique <ph.pereira.7@gmail.com>
2020-11-24 17:55:42 -0300

    Primeiro requisito

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
-  // Implemente aqui
+  const myObj = { method: 'GET' };
+  fetch('https://api.mercadolibre.com/sites/MLB/categories', myObj)
+    .then((resolve) => resolve.json())
+    .then((data) => data);
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const myObj = { method: 'GET' };
+  fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`, myObj)
+    .then((resolve) => resolve.json())
+    .then((data) => data);
 }
