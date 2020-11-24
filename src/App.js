d4ff4cd5b8b8789a0245ae5caf4610c6fd49fdc5
moraisgabri <gabriel8azevedo@gmail.com>
2020-11-24 18:23:04 -0300

    fixing issues

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import HomeInitial from './Pages/HomeInitial';
-import ShoppingCart from './Pages/ShoppingCart'
+import React, { Component } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomeInitial from "./Pages/HomeInitial";
+import ShoppingCart from "./Pages/ShoppingCart";
 // import * as api from '../services/api';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={HomeInitial}/>
-        <Route path="/shopping-cart" component={ShoppingCart} />
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={ HomeInitial } />
+          <Route path="/shopping-cart" component={ ShoppingCart } />
+        </Switch>
       </BrowserRouter>
-    )
+    );
   }
 }
+
 export default App;
