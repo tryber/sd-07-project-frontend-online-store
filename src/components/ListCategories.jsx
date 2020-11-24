4a25550c78452d499843f5c773b0ea69e99688bc
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-11-24 18:03:40 -0300

    ajustando identação

--- src/components/ListCategories.jsx
+++ src/components/ListCategories.jsx
@@ -2,37 +2,37 @@ import * as api from '../services/api';
 import React, { Component } from 'react';
 
 class ListCategories extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            categories: [],
-        }
+    this.state = {
+    categories: [],
     }
+  }
 
-    componentDidMount() {
-        api.getCategories()
-            .then(categories => this.setState({ categories }))
-            .catch(err => console.log(err));
-    }
+  componentDidMount() {
+    api.getCategories()
+    .then(categories => this.setState({ categories }))
+    .catch(err => console.log(err));
+  }
 
 
 
-    render() {
-        return (
-            <section>
-                <select>
-                    {this.state.categories.map(categorie =>
-                    <option
-                        data-testid="category"
-                        key={categorie.id}
-                    >
-                        {categorie.name}
-                    </option>)}
-                </select>
-            </section>
-        );
-    }
+  render() {
+    return (
+    <section>
+        <select>
+        {this.state.categories.map(category =>
+            <option
+            data-testid="category"
+            key={category.id}
+            >
+            {category.name}
+            </option>)}
+        </select>
+    </section>
+    );
+  }
 }
 
 export default ListCategories;
