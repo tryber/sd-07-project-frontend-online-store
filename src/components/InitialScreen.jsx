0db74174c2ff0164604772335bcb3bb3fed80b69
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-11-24 19:51:25 -0300

    Resolução requisito 5

--- src/components/InitialScreen.jsx
+++ src/components/InitialScreen.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 import { SideBar } from './index';
 import './InitialScreen.css';
-import KartImg from '../img/kart.svg'
+import KartImg from '../img/kart.svg';
+import ProductCard from './ProductCard';
 
 import * as api from '../services/api';
 
@@ -11,17 +12,34 @@ class InitialScreen extends React.Component {
   constructor() {
     super();
     this.fetchProducts = this.fetchProducts.bind(this);
+    this.fetchAPI = this.fetchAPI.bind(this);
     this.onInputSearchChange = this.onInputSearchChange.bind(this);
+    this.buttonSearch = this.buttonSearch.bind(this);
     this.state = {
       searchInput: '',
-      products: [],
       categories: [],
+      products: [],
       message: 'Digite algum termo de pesquisa ou escolha uma categoria.',
     };
   }
 
   async componentDidMount() {
     await this.fetchAPI();
+  }
+
+  onInputSearchChange({ target }) {
+    this.setState({ searchInput: target.value });
+  }
+
+  buttonSearch() {
+    this.fetchProducts();
+  }
+
+  async fetchProducts() {
+    const { searchInput } = this.state;
+    const dataInputSearch = searchInput;
+    const getProducts = await api.getProductsFromCategoryAndQuery('query', dataInputSearch);
+    this.setState({ products: getProducts.results });
   }
 
   async fetchAPI() {
@@ -32,28 +50,33 @@ class InitialScreen extends React.Component {
     });
   }
 
-  async fetchProducts() {
-    const dataInputSearch = this.state.searchInput;
-    const getProducts = await api.getProductsFromCategoryAndQuery('query', dataInputSearch);
-    this.setState({ products: getProducts.results });
-  }
-
-  onInputSearchChange({ target }) {
-    this.setState({ searchInput: target.value });
-    this.fetchProducts()
-  }
-
   render() {
-    const { message, searchInput } = this.state;
+    const { message, searchInput, products } = this.state;
     return (
       <div className="container">
         <div className="side-bar">
           <SideBar />
         </div>
-        <div className="container-initial-screen">
-          <input type="text" name="inputSearch" value={searchInput} onChange={this.onInputSearchChange} />
-          <Link data-testid="shopping-cart-button" to={'/kart'}><img src={KartImg} /></Link>
+        <header className="container-initial-screen">
+          <input
+            data-testid="query-input"
+            type="text"
+            name="inputSearch"
+            value={searchInput}
+            onChange={this.onInputSearchChange}
+          />
+          <button data-testid="query-button" type="submit" onClick={this.buttonSearch}>
+            Buscar
+          </button>
+          <Link data-testid="shopping-cart-button" to="/kart">
+            <img src={KartImg} alt="Botão carrinho de compras" />
+          </Link>
           <h1 data-testid="home-initial-message">{message}</h1>
+        </header>
+        <div>
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
         </div>
       </div>
     );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class ProductCard extends React.Component {
-  constructor() {
-    super();
-  }
-
   render() {
+    const { product } = this.props;
+    const { thumbnail, title, price } = product;
     return (
-      <>Cards</>
-    )
+      <div data-testid="product">
+        <img src={thumbnail} alt="Imagem do produto" />
+        <h4>{title}</h4>
+        <h3>{price}</h3>
+      </div>
+    );
   }
 }
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    thumbnail: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default ProductCard;
