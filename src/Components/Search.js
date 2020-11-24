c5a283035a6fd722728cea27a3216ba3430e23d7
Breno Alves <69821386+brenoalv3s@users.noreply.github.com>
2020-11-24 19:35:32 -0300

    Revert "[Grupo 8] Botão do carrino de compras "

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 class Search extends React.Component {
   render() {
 	  return (
 	    <div>
 				<p data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</p>
-				<Link data-testid="shopping-cart-button" to="/cartBuy"><img className="cartBuy"
-        src="https://img.icons8.com/ios/452/shopping-cart.png"
-        alt="imagem de carrinho" /></Link>
 			</div>
 		);
 	}
