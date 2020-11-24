5fb6cd3cbf5525df83e3ab8dd8719c03fd46583a
Victor Junior <victorjr.muchacho@gmail.com>
2020-11-24 19:49:56 -0300

    iniciando o requisito

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import * as api from './services/api.js';
 
 class ProductList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: ''
+    }
+
+  }
   render() {
     return (
       <div>
-        <input />
+        <input data-testid="query-input" value={this.state.inputValue}/>
+        <button data-testid="query-button">Pesquisar</button>
+        <div>
+
+        </div>
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
