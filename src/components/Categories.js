26bac17acbce68d8c982c0a2a4b1f136788a1dce
vladimirrchagas <vladimirrchagas@gmail.com>
2020-11-24 17:07:54 -0300

    Requisito 4 - Adicionar Filtro de Categorias

---
+++ src/components/Categories.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import * as categoryAPI from '../services/api';
+
+import Loading from './Loading';
+
+class Categories extends Component {
+  constructor() {
+    super();
+
+    this.fetchCategories = this.fetchCategories.bind(this);
+
+    this.state = {
+      loading: true,
+      categories: [],
+    }
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+  }
+
+  fetchCategories() {
+    this.setState({loading: true},
+      async () => {
+        const categoriesRet = await categoryAPI.getCategories();
+        this.setState({categories: categoriesRet, loading: false});
+      }
+    );
+  }
+
+  render() {
+    const { categories, loading } = this.state;
+
+    return (
+      loading ? <Loading /> :
+      <div className="categories">
+        <h3>Categorias</h3>
+        <ul>
+          {categories
+            .map(({ id, name }) => (
+            <li key={id}>
+              <input
+                type="radio"
+                id={id}
+                name="category"
+                data-testid="category"
+              />
+              <label htmlFor={id}>
+                {name}
+              </label>
+            </li>
+            ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Categories;
