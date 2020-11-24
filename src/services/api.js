6230a69b514aab0fb5123e14f92bd845889021b6
Lucas Andrade <lafdesouza2002@outlook.com>
2020-11-24 16:51:15 -0300

    Iniciando projeto

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,3 @@
-export async function getCategories() {
-  // Implemente aqui
-}
+export async function getCategories() {}
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
-}
+export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {}
