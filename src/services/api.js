cf308ee33341876d6d97d73d0514a3ee3d6e6b76
Marcos R. Silva <marcos.1998226@gmail.com>
2020-11-24 19:08:26 -0300

    Solving github Problems

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 export async function getCategories() {
   const response = await fetch(
-    'https://api.mercadolibre.com/sites/MLB/categories'
+    'https://api.mercadolibre.com/sites/MLB/categories',
   );
   const category = await response.json();
   return category;
@@ -8,7 +8,7 @@ export async function getCategories() {
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const response = await fetch(
-    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`
+    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`,
   );
   const products = await response.json();
   return products;
