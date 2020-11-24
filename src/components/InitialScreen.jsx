5b9cea68fd325027faf0b1be51faccf786a67b59
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-11-24 14:59:21 -0300

    Resolução requisito 2

--- src/components/InitialScreen.jsx
+++ src/components/InitialScreen.jsx
@@ -4,14 +4,16 @@ class InitialScreen extends React.Component {
   constructor() {
     super();
     this.state = {
-      message: 'Digite....'
-    }
+      message: 'Digite algum termo de pesquisa ou escolha uma categoria.',
+    };
   }
 
   render() {
+    const { message } = this.state;
     return (
       <div>
-        <h1>{this.state.message}</h1>
+        <input type="text" />
+        <h1 data-testid="home-initial-message">{message}</h1>
       </div>
     );
   }
