d531fa73c87c1684a3ae6874c78398dc67e61a2f
Arnaelcio <arnaelcio.gomes@gmail.com>
2020-11-24 18:59:48 -0300

    ajeitando Lint

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,12 @@
 export async function getCategories() {
-  const end = await fetch("https://api.mercadolibre.com/sites/MLB/categories");
+  const end = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   const end2 = await end.json();
 
   return end2;
 }
 getCategories();
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const end = await fetch(
-    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`
-  );
+  const end = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
   const end2 = await end.json();
 
   return end2;
