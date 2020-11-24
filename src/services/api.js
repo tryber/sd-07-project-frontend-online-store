9d8dabef26ae6798f035a0693059a485f1f6fe64
Eriquy Jhordan <eriquy@gmail.com>
2020-11-24 19:09:55 -0300

    fix issues lint

--- src/services/api.js
+++ src/services/api.js
@@ -4,17 +4,17 @@ export async function getCategories() {
     const categories = await request.json();
     return categories;
   } catch (error) {
-    throw new Error(`There was an error making the request: ${error}`);  
+    throw new Error(`There was an error making the request: ${error}`);
   }
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   try {
-    const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=
-      ${categoryId}&q=${query}`);
+    const request = await fetch(`https://api.mercadolibre.com/sites/MLB/
+      search?category=${categoryId}&q=${query}`);
     const categories = await request.json();
     return categories;
   } catch (error) {
-    throw new Error(`There was an error making the request with id and query: ${error}`);  
+    throw new Error(`There was an error making the request with id and query: ${error}`);
   }
 }
