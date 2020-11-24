4e48be03dcd9a62b5fb58e71a9595266e885fd12
Andre Menezes <dehhmenezes87@gmail.com>
2020-11-24 17:25:33 -0300

    criar pagina de listagem de produtos vazia

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import SearchBar from './Components/SearchBar';
+
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* logo */}
+        <SearchBar />
+        {/* filtro */}
+        {/* carrinho */}
+      </div>
+    )
+  }
+}
+
+export default Header;
