dc47347b1e64e9502c01cb6f76c01c35891a9439
alvaro <alvarovascon2@gmail.com>
2020-11-24 16:11:41 -0300

    implementando funçao getCategories

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 export async function getCategories() {
-  // Implemente aqui
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((response) => response.json());
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  
 }
