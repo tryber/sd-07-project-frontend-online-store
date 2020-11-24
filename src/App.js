9a02f01250746e3314ae1c43036a0ea186a19318
Marcos R. Silva <marcos.1998226@gmail.com>
2020-11-24 18:59:50 -0300

    Solving github Problems

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import React, {Component} from 'react';
-import * as api from './services/api'
+import React, { Component } from 'react';
 
 class App extends Component {
-  render(){
-    return (
-      <div>
-        {
-          
-        }
-      </div> 
-      )
+  render() {
+    return <div>hello world</div>;
   }
 }
 
