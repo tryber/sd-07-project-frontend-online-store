5cfb2226025c50822058faf4a5ced2a1f93c2cde
Eduardo Pizorno <pizorno@gmail.com>
2020-11-24 10:51:36 -0300

    Api implementada

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  const Request = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const Json = await Request.json();
+  return Json;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const Request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const Json = await Request.json();
+  return Json;
 }
