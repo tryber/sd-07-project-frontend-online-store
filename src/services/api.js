451bbaa3475642922876360cece21c1141808a50
Lorena Góes <goesloren@gmail.com>
2020-11-24 17:23:10 -0300

    Definindo o estado inicial da chamada à Api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,45 @@
 export async function getCategories() {
   // Implemente aqui
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+  try {
+    const response = await fetch(endpoint);
+    const categories = await response.json();
+    console.log(categories);
+    return categories;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const endpoint = ' https://api.mercadolibre.com/sites/MLB/search?';
+  try {
+    // Buscar itens por termo e categoria:
+    if(categoryId && query) {
+      const url = `${endpoint}category=${categoryId}&q=${query}`;
+      const response = await fetch(url);
+      const categories = await response.json();
+      console.log(categories);
+      return categories;
+    }
+    // Buscar itens por termo:
+    if (categoryId && query === 'undefined') {
+      const url = `${endpoint}category=${categoryId}`;
+      const response = await fetch(url);
+      const categories = await response.json();
+      console.log(categories);
+      return categories;
+    }
+    // Buscar itens por categoria:
+    if (categoryId === 'undefined' && query) {
+      const url = `${endpoint}q=${query}`;
+      const response = await fetch(url);
+      const categories = await response.json();
+      console.log(categories);
+      return categories;
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
