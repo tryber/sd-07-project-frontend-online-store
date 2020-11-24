aeb406bbae4e30871cd020d61d0802781bc17ee8
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-11-24 19:14:45 -0300

    Refactoring

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,14 @@
+
 export async function getCategories() {
-  const endPoint = 'https://api.mercadolibre.com/sites/MLB/categories';
-  const request = await fetch(endPoint);
-  const object = await request.json();
-  return object;
+  return new Promise((resolve) => {
+    const endPoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+    fetch(endPoint).then((request) => request.json()).then((object) => resolve(object));
+  });
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const endPoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
-  const request = await fetch(endPoint);
-  const object = await request.json();
-  return object;
+  return new Promise((resolve) => {
+    const endPoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+    fetch(endPoint).then((request) => request.json()).then((object) => resolve(object));
+  });
 }
