64c59ab34bbad63aef76864010cd7e6cbe9fe1a4
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-11-24 18:53:48 -0300

    Criando página ShoppingCart.

---
+++ src/pages/ShoppingCart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ShoppingCart extends Component {
+  render() {
+    return (
+      <div>
+        Seu carrinho está vazio
+      </div>
+    );
+  }
+}
+
+export default ShoppingCart;
