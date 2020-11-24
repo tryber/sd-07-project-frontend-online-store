c55e6c3117cf403492f102b841452e6302f19833
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-11-24 13:57:25 -0300

    criação componente InitialScreen e rotas

---
+++ src/components/InitialScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class InitialScreen extends React.Component {
+//   constructor() {
+//     super();
+//   }
+
+  render() {
+    return (
+      <div>
+        <h1>teste</h1>
+      </div>
+    );
+  }
+}
+
+export default InitialScreen;
