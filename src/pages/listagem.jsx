662f9c4eeb88b0436c484d25825e41bfe17db468
adrianoForcellini <adrianoforcellinipedretti@gmail.com>
2020-11-25 16:34:30 -0300

    quaseLa

--- src/pages/listagem.jsx
+++ src/pages/listagem.jsx
@@ -6,7 +6,9 @@ class Listagem extends Component {
       <div>
         <input type="text" id="list"></input>
         <label htmlFor="list" data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</label>
-      </div>
+        <a href = "carrinho.jsx"><img src="https://seeklogo.com/images/C/Carrinho_de_Compras-logo-F251151A71-seeklogo.com.png" width="50" height="50" data-testid="shopping-cart-button"></img></a>
+     </div>
+      
     )
   }
 }
