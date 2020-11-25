1df9fe1c3a710b1b60b05c2789287806cd900210
adrianoForcellini <adrianoforcellinipedretti@gmail.com>
2020-11-25 18:49:26 -0300

    Carrinho botao + Janela adjacente

--- src/pages/listagem.jsx
+++ src/pages/listagem.jsx
@@ -6,7 +6,7 @@ class Listagem extends Component {
       <div>
         <input type="text" id="list"></input>
         <label htmlFor="list" data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</label>
-        <a href = "carrinho.jsx"><img src="https://seeklogo.com/images/C/Carrinho_de_Compras-logo-F251151A71-seeklogo.com.png" width="50" height="50" data-testid="shopping-cart-button"></img></a>
+        <a href = "/carrinho"><img src="https://seeklogo.com/images/C/Carrinho_de_Compras-logo-F251151A71-seeklogo.com.png" width="50" height="50" data-testid="shopping-cart-button"></img></a>
      </div>
       
     )
