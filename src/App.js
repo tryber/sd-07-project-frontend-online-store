a131f23ba26b20d9487d62309568e76c816b17e7
Andre Serudo <andreserudo@gmail.com>
2020-11-25 17:22:06 -0400

    criando arquivos base do requisito

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import CategoriesList from './pages/CategoriesList';
+import ProductDetail from './pages/ProductDetail';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
           <Route exact path="/" component={CategoriesList} />
+          <Route exact path="/product/:id" component={ProductDetail} />
       </BrowserRouter>
     </div>
   );
