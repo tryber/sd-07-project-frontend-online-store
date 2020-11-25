3cfd6951a89ae65dfeed64741ea24a0afcd10ec2
Raphael Caputo <faelelf2@gmail.com>
2020-11-25 18:22:16 -0300

    Adicionando renderizacao do requisito 4

--- src/Components/CategoryList.jsx
+++ src/Components/CategoryList.jsx
@@ -3,34 +3,40 @@ import * as API from '../services/api';
 
 class CategoryList extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.fetchAPI = this.fetchAPI.bind(this);
 
     this.state = {
       categories: [],
-    }
+    };
   }
-  
+
   componentDidMount() {
-    this.fetchAPI()
+    this.fetchAPI();
   }
 
   async fetchAPI() {
     const result = await API.getCategories();
     this.setState({
       categories: result,
-    })
+    });
   }
 
-  render () {
+  render() {
     const { categories } = this.state;
     return (
       <div>
         <span>Categorias</span>
-        <ul>{categories.map((category) => <li data-testid="category">{category.name}</li>)}</ul>
+        <ul>
+          {categories.map((category) => (
+            <li data-testid="category" key={ category.id }>
+              {category.name}
+            </li>
+          ))}
+        </ul>
       </div>
-    )
+    );
   }
 }
 
