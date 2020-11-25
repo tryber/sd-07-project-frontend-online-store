0aa9219a5e770230a4d914f2eb692536705196ff
Lfocosi <luccafocosi@gmail.com>
2020-11-25 09:19:00 -0300

    troquei o then cath por async await

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -4,16 +4,19 @@ import * as api from '../services/api';
 class Categories extends Component {
   constructor() {
     super();
-
+    this.get.bind(this);
     this.state = {
       categories: [],
     };
   }
 
   componentDidMount() {
-    api.getCategories()
-      .then((categories) => this.setState({ categories }))
-      .catch((error) => console.log(error));
+    this.get();
+  }
+
+  async get() {
+    const categories = await api.getCategories();
+    this.setState({ categories });
   }
 
   render() {
