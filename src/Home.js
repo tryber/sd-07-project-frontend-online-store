af8a3d20c9266afc34cb92512f43b9dec3207e7f
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-11-25 12:49:27 -0300

    listando categorias na página principal

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Sidebar from './components/Sidebar';
 
 class Home extends React.Component {
   render() {
     return (
-      <p data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </p>
+      <div>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <Sidebar />
+      </div>
     );
   }
 }
