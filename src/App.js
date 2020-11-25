d26c2cd81aa784e628fbc10106f229ac782938fd
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-11-25 15:36:11 -0300

    Criado a pagina main-page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './App.css';
-import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
-import SearchBar from './components/SearchBar';
+/* import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined'; */
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+/* import SearchBar from './components/SearchBar';
+import ShoppingCart from './components/ShoppingCart'; */
+import MainPage from './pages/main-page';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Route exact path="/" component={ SearchBar } />
-        <Route exact path="/shopping-cart" component={ ShoppingCart } />
-        <Link to="/shopping-cart">
-          <ShoppingCartOutlinedIcon data-testid="shopping-cart-button" />
-        </Link>
-        <Route path="/" component={ SearchBar } />
+        <Switch>
+          <Route exact path="/" component={ MainPage } />
+          <Route exact path="/shopping-cart" component={ ShoppingCart } />
+        </Switch>
       </BrowserRouter>
     </div>
   );
