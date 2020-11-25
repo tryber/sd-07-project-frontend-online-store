7f83aa711e05416292923ee8f03250d3e1628116
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-11-25 18:19:24 -0300

    Update CategoryList.jsx

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -23,14 +23,14 @@ export default class CategoryList extends React.Component {
   }
 
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <div className="category-list">
           <h1>Categorias</h1>
-          {this.state.categories
-            .map((category) => 
-              <CategoryItem
-                key={category.id} category={category.name} />)}
+          {categories.map((category) => 
+             <CategoryItem
+               key={category.id} category={category.name} />)}
         </div>
       </div>
     );
