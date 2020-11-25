c6e70c6bf79605f6821a154ab9bd53d36eb25ae2
Anderson Meira <contatodeanderson@gmail.com>
2020-11-25 11:32:10 -0300

    Requisito 3 - Criar página do carrinho de compras

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class Home extends React.Component {
         <h3 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h3>
+        <Link to="/Cart" data-testid="shopping-cart-button">Carrinho</Link>
       </div>
     );
   }
