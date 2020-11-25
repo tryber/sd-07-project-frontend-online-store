78d854a5c2f3a1796e4e37da2298720d34380b1d
gabrieldeori <deori.pessoal@gmail.com>
2020-11-25 00:11:34 -0300

    Correcting import of CategoryList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ProductList from './components/ProductList';
-import CategoryList from './CategoryList';
+import CategoryList from './components/CategoryList;
 
 function App() {
   return (
