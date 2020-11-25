4351070b6df333023dd9d73408b93407912b62cc
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-11-25 11:22:37 -0300

    Adiciona CSS para diminuir tamanho dos ícones

--- src/Components/ShoppingCartButton.jsx
+++ src/Components/ShoppingCartButton.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './ShoppingCartButton.css';
 
 
 class ShoppingCartButton extends React.Component {
@@ -7,11 +8,12 @@ class ShoppingCartButton extends React.Component {
     return (
       <Link to="/shopping-cart" data-testid="shopping-cart-button">
         <img
+          className="shopping-cart-icon"
           src="https://www.flaticon.com/svg/static/icons/svg/1170/1170678.svg"
           alt="imagem com um carrinho de compras"
         />
       </Link>
-      );
+    );
   }
 }
 
--- src/Pages/HomeInitial.jsx
+++ src/Pages/HomeInitial.jsx
@@ -5,10 +5,12 @@ class HomeInitial extends Component {
   render() {
     return (
       <div className="home-page">
-      <ShoppingCartButton className="shopping-cart-button"/>
-      <span data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</span>
+        <span data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </span>
+        <ShoppingCartButton className="shopping-cart-button" />
       </div>
-    )
+    );
   }
 }
 
