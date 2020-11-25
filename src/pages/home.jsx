f6470e2045459cae39f1a44dc9a561c5397f5b9b
lisvalver <lisvalver.atte@gmail.com>
2020-11-25 16:58:00 -0300

    Adicionando CategoryList na Home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Category from '../components/categoryList';
 import SearchBar from '../components/searchBar';
 
 class Home extends React.Component {
@@ -7,6 +8,7 @@ class Home extends React.Component {
     return(
       <div>
         <SearchBar />
+        <Category />
         <Link to="/shoppingCart" data-testid="shopping-cart-button">Carrinho de compras</Link>
       </div>
     )
