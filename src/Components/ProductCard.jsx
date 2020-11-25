50b2d210546facde62fc2ed808efb40acdd984ed
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-11-25 17:37:10 -0300

    Requisito 5 - SearchProduct

---
+++ src/Components/ProductCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ProductCard extends React.Component {
+  render() {
+    const { product } = this.props;
+    const { title, thumbnail, price } = product;
+    return (
+      <div data-testid="product">
+        <div>{ title }</div>
+        <div>{ price }</div>
+        <img src={ thumbnail } alt="exemplar do produto" />
+      </div>
+    );
+  }
+}
+
+ProductCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  thumbnail: PropTypes.string.isRequired,
+  price: PropTypes.string.isRequired,
+};
+
+export default ProductCard;
