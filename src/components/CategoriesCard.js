2a10a6134efb9905052037f30050b7be94dc0100
spawnred <bruno.spawnhell@gmail.com>
2020-11-25 01:30:02 -0300

    [GRUPO 11] item 5

--- src/components/CategoriesCard.js
+++ src/components/CategoriesCard.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 class CategoriesCard extends React.Component {
   render() {
+    const { product } = this.props;
+    const { thumbnail, title, price } = product;
     return (
-      <div />
+      <div data-testid="product">
+        <img src={ thumbnail } alt="Imagem do produto" />
+        <h2>{ title }</h2>
+        <h2>{ price }</h2>
+      </div>
     );
   }
 }
+
+CategoriesCard.propTypes = {
+  product: PropTypes.array }.isRequired;
 
 export default CategoriesCard;
--- src/pages/CategoriesList.js
+++ src/pages/CategoriesList.js
@@ -1,17 +1,87 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import CategoriesCard from '../components/CategoriesCard';
+
+
+import * as api from '../services/api';
 
 class CategoriesList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: [],
+      // categoryId: '',
+      query: '',
+      object: [],
+    };
+    this.CategoriesList = this.CategoriesList.bind(this);
+    this.onInputSearchChange = this.onInputSearchChange.bind(this);
+    this.SearchProduct = this.SearchProduct.bind(this);
+  }
+
+  componentDidMount() {
+    this.CategoriesList();
+  }
+
+  onInputSearchChange({ target }) {
+    this.setState({ query: target.value });
+  }
+
+  SearchProduct() {
+    const { query } = this.state;
+    const categoryId = 'ALL';
+    console.log(query);
+    api.getProductsFromCategoryAndQuery(categoryId, query)
+      .then((object) => {
+        // console.log(object);
+        this.setState({ object });
+      })
+      .catch((error) => console.log('Promises rejected: ', error));
+  }
+
+
+  CategoriesList() {
+    api.getCategories()
+      .then((categories) => {
+        // console.log(categories);
+        this.setState({ categories });
+      })
+      .catch((error) => console.log('Promises rejected: ', error));
+  }
+
   render() {
+    const { object } = this.state;
+    const { query } = this.state;
+    console.log(object);
+    console.log(query);
     return (
       <div className="home-initial">
         <div className="home-initial-input">
           <label htmlFor="home-initial-message">
-            <input className="input" />
-            <h3 data-testid="home-initial-message">
-              Digite algum termo de pesquisa ou escolha uma categoria.
-            </h3>
+            <input
+              className="input"
+              data-testid="query-input"
+              placeholder="Pesquisar"
+              value={ query }
+              onChange={ this.onInputSearchChange }
+            />
+            <button
+              data-testid="query-button"
+              type="submit"
+              onClick={ this.SearchProduct }
+            >
+              Pesquisar
+            </button>
           </label>
+          <section>
+            {object.map((product) => (
+              <CategoriesCard key={ product.id } product={ product } />
+            ))}
+          </section>
+          <h3 data-testid="home-initial-message">
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h3>
         </div>
         <Link
           to="/shoopingcart"
@@ -19,7 +89,7 @@ class CategoriesList extends Component {
           className="home-initial-link"
         >
           <img
-            src="images/shopping-cart-50.png"
+            src="images/icons-shopping-cart.png"
             alt="Carrinho de Compras"
           />
         </Link>
