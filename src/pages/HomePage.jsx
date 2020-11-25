ace8ff3254c385208ee557ccac3968c77947737c
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-11-25 15:57:02 -0300

    Implementation-Shopping-Cart-Button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import SearchBar from '../components/SearchBar/SearchBar';
 import ProductList from '../components/ProductList/ProductList';
+import ShoppingCartButton from  '../components/ShoppingCartButton/ShoppingCartButton'
 
 class HomePage extends Component {
     render() {
       return (
         <div>
           <SearchBar />
+          <ShoppingCartButton/>
           <ProductList />
         </div>
       )
