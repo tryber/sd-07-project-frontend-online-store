f239f924f7e4bcede43296967947f5c82e257046
Emerson Moonlit <emoonlit@pop-os.localdomain>
2020-11-25 17:29:39 -0300

    Create general component Button

---
+++ src/components/forms/Button/style.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-color: #fff;
+  border: 1px solid black;
+  padding: 5px 10px;
+  transition: all ease 0.2s;
+  outline: none;
+  transform: scale(0.9);
+  transform: all ease 0.2s;
+
+  width: ${(props) => props.width };
+ :hover {
+     background-color: #f5f5f5;
+     border: 1px solid dodgerblue;
+     box-shadow: 0 0 5px 0 dodgerblue;
+ }
+ :active {
+    border: 1px solid green;
+    box-shadow: 0 0 5px 0 green;
+    transform: scale(1);
+ }
+
+`;
