325c1484f76c58afddd389ecbf0c3dd3adeab6b1
Breenolf <blf@ic.ufal.br>
2020-11-25 19:34:39 -0300

    Priority 1- Requirement 4 - Listing categories

---
+++ src/components/CategoryList/CategoryList.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import * as api from "../../services/api";
+
+class CategoryList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categoriesList: [],
+      categoryIdSelected: "",
+    };
+    this.fecthList = this.fecthList.bind(this);
+  }
+
+  componentDidMount() {
+    this.fecthList();
+  }
+
+  fecthList() {
+    api
+      .getCategories()
+      .then((categories) => this.setState({ categoriesList: categories }));
+  }
+
+  render() {
+    const { categoriesList } = this.state;
+    const { onChangeCategory } = this.props;
+    return (
+      <div>
+        <span>Categorias:</span>
+        <div>
+          {categoriesList.map((category) => (
+            <label key={ category.id } htmlFor={ category.id }>
+              <input
+                onClick={ onChangeCategory }
+                name="category"
+                type="radio"
+                id={ category.id }
+                key={ category.id }
+                data-testid="category"
+              />
+              {category.name}
+            </label>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CategoryList;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import SearchBar from '../components/SearchBar/SearchBar';
 import ProductList from '../components/ProductList/ProductList';
+import CategoryList from '../components/CategoryList/CategoryList';
 
 class HomePage extends Component {
-    render() {
-      return (
-        <div>
-          <SearchBar />
-          <ProductList />
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div>
+        <ProductList />
+        <CategoryList onChangeCategory={this.onChangeCategory } />
+      </div>
+    )
   }
-  
-  export default HomePage;
-  
+}
+
+export default HomePage;
