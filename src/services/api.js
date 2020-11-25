3f7a7643b94b4eb4d8895302a8872cdd31fe0db0
dhiegodias <67385608+dhiegodias@users.noreply.github.com>
2020-11-24 21:19:15 -0300

    Requisito API ML

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,13 @@
 export async function getCategories() {
-  // Implemente aqui
+  const result = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const convertJson = await result.json();
+
+  return convertJson;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const fullEndPoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const fullResponse = await fetch(fullEndPoint);
+  const fullConvertJson = fullResponse.json();
+  return fullConvertJson;
 }
