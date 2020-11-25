c3960c08aabd609560aad4f61f045b3efa8f6f31
FelipeAVieiraTurma7 <veiculozeroemissao@gmail.com>
2020-11-25 13:13:14 -0300

    colocando componente Home na pasta de componentes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Home from './Home';
+import Home from './components/Home';
 
 function App() {
   return (
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Sidebar from './components/Sidebar';
+import Sidebar from './Sidebar';
 
 class Home extends React.Component {
   render() {
