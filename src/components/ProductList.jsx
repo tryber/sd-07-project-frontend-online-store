df49377858ab0691c1e77af25e35ba8eb573fbd6
Anderson Santana <anderslash@gmail.com>
2020-11-25 18:02:43 -0300

    Adicao da funcionalidade de clique para detalhes

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import '../style/productList.css';
 
 class ProductList extends Component {
   render() {
     const { product } = this.props;
-    const { title, thumbnail, price } = product;
+    const { id, title, thumbnail, price } = product;
 
     return (
-      <div data-testid="product">
-        <h4>{title}</h4>
-        <img src={ thumbnail } alt="Produto listado" />
-        <p>{price}</p>
-      </div>
+      <Link to={ `/${id}` } data-testid="product-detail-link">
+        <div data-testid="product">
+          <h4>{title}</h4>
+          <img src={ thumbnail } alt="Produto listado" />
+          <p>{price}</p>
+        </div>
+      </Link>
     );
   }
 }
 
 ProductList.propTypes = {
   product: PropTypes.shape({
+    id: PropTypes.string,
     title: PropTypes.string,
     thumbnail: PropTypes.string,
     price: PropTypes.number,
