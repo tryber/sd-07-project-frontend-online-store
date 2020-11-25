4a74b27250e584c0ece4ae7d40b34d8760f7b9d9
kramersilva <kramerscs@gmail.com>
2020-11-24 22:47:25 -0300

    requisito3 cart

---
+++ src/pages/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text"/>
+        <span data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</span>
+        <Link data-testid="shopping-cart-button" to="/cart">
+          <i className="fas fa-shopping-cart"></i>
+        </Link>
+      </div>
+    );
+  }
+}
+
+export default Main;
