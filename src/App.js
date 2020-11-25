b1f2dba559f06707991e3104e7a777ecc87df89c
afonsofn <afonsoafn@gmail.com>
2020-11-25 08:49:26 -0300

    Resolvendo Lint

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './App.css';
-import Home from './pages/Home'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import ShoppingCart from './pages/ShoppingCart'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-          <Switch>
-            <Route path="/pages/ShoppingCart" component={ ShoppingCart } />
-            <Route exact path="/" component={ Home } />
-          </Switch>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/pages/ShoppingCart" component={ ShoppingCart } />
+          <Route exact path="/" component={ Home } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react';
+import SearchBar from '../components/SearchBar';
 import ShoppingCartButton from '../components/ShoppingCartButton'
-
-class Home extends React.Component {
+    
+export default class Home extends Component {
   render() {
     return (
       <div>
         <header>
           <h1>Home</h1>
+          <SearchBar />
           <ShoppingCartButton />
         </header>
       </div>
-    )
+    );
   }
 }
-
-export default Home;
