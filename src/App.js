3f81195a59be9a4096336a45d9b132d3b7db2d52
kramersilva <kramerscs@gmail.com>
2020-11-24 23:01:55 -0300

    fix semi colon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Main from './pages/Main';
-import Cart from './pages/Cart'
+import Cart from './pages/Cart';
 
 import './App.css';
 
