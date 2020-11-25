4ea0df3cd74fffc512ff2a0eeb5310aeeb020f2c
IsabelaCardoso <pcardoso.isabela@gmail.com>
2020-11-25 16:23:24 -0300

    Adiciona ProductCard

--- src/Pages/HomeInitial.jsx
+++ src/Pages/HomeInitial.jsx
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
 import ShoppingCartButton from '../Components/ShoppingCartButton';
+import * as api from '../services/api';
+import SearchBar from '../Components/SearchBar';
 
 class HomeInitial extends Component {
+  constructor() {
+    super();
+    this.searchEventHandler = this.searchEventHandler.bind(this);
+    this.searchOnChange = this.searchOnChange.bind(this);
+    this.state = {
+      productArr: [],
+      searchBarValue: undefined,
+    };
+  }
+
+  async searchEventHandler() {
+    const { searchBarValue } = this.state;
+    const dataArr = await api.getProductsFromCategoryAndQuery(searchBarValue);
+    this.setState({
+      productArr: dataArr,
+    });
+  }
+
+  searchOnChange() {
+    this.setState({ searchBarValue: Eventarget.value });
+    console.log(this.state.searchBarValue);
+  }
+
   render() {
+    const { productArr } = this.state;
     return (
       <div className="home-page">
+        <ShoppingCartButton className="shopping-cart-button" />
         <span data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </span>
-        <ShoppingCartButton className="shopping-cart-button" />
+        <SearchBar
+          searchEventHandler={ this.searchEventHandler }
+          searchOnChange={ this.searchOnChange }
+        />
       </div>
     );
   }
