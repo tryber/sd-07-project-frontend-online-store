323b63afec130e415b2b3c19e659e3452e4f9164
Mauricio Hamaji Homma <43273469+mhamaji@users.noreply.github.com>
2020-11-25 17:05:33 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import Products from './pages/Products';
-import ShoppingCart from './pages/ShoppingCart';
 import CategoryList from './components/CategoryList';
 
 import './App.css';
