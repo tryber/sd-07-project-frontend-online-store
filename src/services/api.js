c52b05a49d9e6512119f8d7b857cd5ab062a36b4
Alstent <rafaelasltent@gmail.com>
2020-11-25 22:58:21 +0100

    requisito 3

--- src/services/api.js
+++ src/services/api.js
@@ -8,9 +8,11 @@ export async function getCategories() {
 }
 
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const CATEGORY_ID = categoryId
+  const searchItem = query
   return new Promise((resolve) => {
-    const categories = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
+    const categories = `https://api.mercadolibre.com/sites/MLB/search?category=${CATEGORY_ID}&q=${searchItem}`;
     const fetchByItem = fetch(categories)
       .then((data) => data.json());
     resolve(fetchByItem);
