b7afeaf2c5f912cd4a2f0df839b3d66bdc1e135c
murilopereira006 <muriloedu006@gmail.com>
2020-11-25 17:16:40 -0300

    buscando retorno da Api pela searchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,6 +12,7 @@ class SearchBar extends React.Component {
     this.state = {
       arrayOfCategories: [],
       loading: true,
+      parameterSearch: '',
     }
   }
 
@@ -27,12 +28,29 @@ class SearchBar extends React.Component {
       });
   }
 
+  getInputString(){
+    this.setState({
+      parameterSearch: valeu
+    })
+  }
+
+  async getApiInfo() {
+    // https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}.json();
+    // onClick query == input
+  }
+
   componentDidMount() {
     this.fetchCategories();
   }
+
+  componentDidUpdate() {
+    this.getApiInfo()
+  }
+
   render() {
     const { arrayOfCategories, loading } = this.state;
     const loadingElement = <span>Carregando...</span>;
+    
     return (
       <div className="header-container">
         <img className="logo" src={logo} alt="logo-react" />
@@ -43,7 +61,7 @@ class SearchBar extends React.Component {
             </div>
           <div className="search-bar-content">
             <img className="search-icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEJTOvaCRMvUxPy8OR3W53CXP_eLOKV3QBaw&usqp=CAU" alt="search icon" />
-            <input data-testid="query-input" className="search-bar" type="text" />
+            <input onChange={getInputString()} data-testid="query-input" className="search-bar" type="text" />
           </div>
           <div className="home-initial-message" data-testid="home-initial-message">
             <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
@@ -53,10 +71,12 @@ class SearchBar extends React.Component {
           </div>
           <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
         </div>
-        <button data-testid="query-button" >só pra ver</button>
+        <button data-testid="query-button" onClick={()=>{this.getApiInfo()}} >Buscar</button>
       </div >
     );
   }
 }
+
+SearchBar.prototype = {}
 
 export default SearchBar;
