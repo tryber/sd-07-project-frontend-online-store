9fb213ade812427e4c9761ca02d1cf2a8c1312b8
gabrieldeori <deori.pessoal@gmail.com>
2020-11-25 00:31:11 -0300

    Returning a checkbox with text in render

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -2,31 +2,45 @@ import React from 'react';
 import * as api from '../services/api'
 
 class CategoryList extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-          categories: [],
-        };
-        this.apiRequisition = this.apiRequisition.bind(this);
-      }
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+    this.apiRequisition = this.apiRequisition.bind(this);
+  }
 
-      componentDidMount() {
-          this.apiRequisition();
-      }
+  componentDidMount() {
+    this.apiRequisition();
+  }
 
-      apiRequisition() {
-        api.getCategories().then((listOfCategories) => {
-          this.setState({
-            categories: listOfCategories,
-          });
-        });
-      }
-    
-    render() {
-        return(
-          <div>teste</div>
-        );
-    }
+  apiRequisition() {
+    api.getCategories().then((listOfCategories) => {
+      this.setState({
+        categories: listOfCategories,
+      });
+    });
+  }
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <div>
+        {categories.map((category) => {
+          return (
+            <label key={category.id} htmlFor="">
+              <input
+                type="checkbox"
+                name={category.name}
+                value={category.name}
+              />
+              {category.name}
+            </label>
+          )}
+        )}
+      </div>
+    );
+  }
 }
 
 export default CategoryList;
