0f90217dc8eff485d6d36180d181dd4f2b25ecbd
Thadeu Castelo Branco Ramos <thadeucbr@gmail.com>
2020-11-25 12:52:10 -0300

    arrumando o lint

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
+
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Route path="/" component={SearchBar} />
+        <Route path="/" component={ SearchBar } />
       </BrowserRouter>
     </div>
   );
