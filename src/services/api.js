0c06ef8a51795aa61c87eb3bbc0f5f253bdbb487
berchon <berchon@gmail.com>
2020-11-25 16:37:00 -0300

    Requito 1 v1.1

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,11 @@ const getFetch = (url) => fetch(url).then((element) => element.json());
 
 export async function getCategories() {
   const url = 'https://api.mercadolibre.com/sites/MLB/categories';
-  const result = await getFetch(url);
-  return result;
+  const promisse = await getFetch(url);
+  return promisse;
 }
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
-  const result = await getFetch(url);
-  return result;
+  const promisse = await getFetch(url);
+  return promisse;
 }
