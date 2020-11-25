e6d4701bb35fc83edf622610982bb52af1ec8518
afonsofn <afonsoafn@gmail.com>
2020-11-25 08:24:49 -0300

    Rotas no App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import ShoppingCart from './pages/ShoppingCart'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/pages/ShoppingCart" component={ ShoppingCart } />
+            <Route exact path="/" component={ Home } />
+          </Switch>
+        </BrowserRouter>
     </div>
   );
 }
