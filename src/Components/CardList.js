5b50eb2d7f9a053ba3cdbd4bc26042505f1f47fa
Strongreen <strongreen@live.com>
2020-11-25 17:11:03 -0300

    enviando arquivos

---
+++ src/Components/CardList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CardProduct from './CardProduct';
+
+class CardList extends React.Component {
+  render() {
+    const { products } = this.props;
+    return (
+      <div>
+        {products.map((product) => (
+          <CardProduct key={product.id} product={product} />
+        ))}
+      </div>
+    );
+  }
+}
+
+CardList.propTypes = { product: PropTypes.arrayOf(PropTypes.objectOf).isRequired };
+
+export default CardList;
---
+++ src/Components/ListAllProducts.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import '../App.css';
+import * as api from '../services/api';
+import CardList from './CardList';
+import ProductNotFound from './ProductNotFound';
+import DigiteTermo from './DigiteTermo';
+
+class ListAllProducts extends React.Component {
+ constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      products: {},
+      status: '',
+      categoryId: 0,
+    };
+    this.getProduct = this.getProduct.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  getProduct() {
+    this.setState( async () => {
+      const { categoryId, term } = this.state;
+      const getingProduct = await api.getProductsFromCategoryAndQuery(
+        categoryId,
+        term,
+      );
+      console.log(getingProduct)
+      if(getingProduct.results.length !== 0){
+        this.setState({ products: getingProduct.results, status: 'OK' });
+      }
+      else{
+        this.setState({ products: getingProduct.results, status: 'Fail' });
+      }
+   
+    });
+  } 
+
+  onSearchChange(event) {
+   const { name, value } = event.target
+   this.setState({ [name]: value });
+  }
+
+  render() {
+    const { term, products, status } = this.state;
+    return (
+      <div>
+        <input
+          data-testid="query-input"
+          className="input-search"
+          type="text"
+          name="term"
+          value={ term }
+          placeholder="Digite algum termo de pesquisa aqui"
+          onChange={ this.onSearchChange }
+        />
+        <button
+          type="button"
+          className="button-search"
+          data-testid="query-button"
+          onClick={ this.getProduct }
+        >
+        Procurar
+        </button>
+        <div>
+          { 
+          status === 'OK' && status !== 'Fail' ? <CardList products={ products } /> : <DigiteTermo /> }
+          {
+          status === 'Fail' ? <ProductNotFound /> : ''
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ListAllProducts;
