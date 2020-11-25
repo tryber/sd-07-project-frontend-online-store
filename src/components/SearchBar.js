77ea843e7731a803c7dad053d4e87b66c7b3407a
cezar88augusto <cezar88augusto@gmail.com>
2020-11-25 20:57:11 -0300

    Requisito 5

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,6 +3,7 @@ import logo from '../logo.svg';
 import { Link } from 'react-router-dom';
 import * as mlAPI from '../services/api';
 import Categories from './Categories';
+import Card from './Card'
 
 
 class SearchBar extends React.Component {
@@ -11,9 +12,18 @@ class SearchBar extends React.Component {
 
     this.state = {
       arrayOfCategories: [],
+      arrayOfItemByInputedText: [],
+      searchElement: '',
+      category: '',
       loading: true,
-      searchParameter: '',
     }
+    this.changeStateValue = this.changeStateValue.bind(this);
+    this.fetchApiByQuery = this.fetchApiByQuery.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchCategories();
+    this.fetchApiByQuery();
   }
 
   async fetchCategories() {
@@ -28,63 +38,58 @@ class SearchBar extends React.Component {
       });
   }
 
-  getInputString(){
-    this.setState({
-      searchParameter: valeu
-    })
-  }
-
-  async getApiInfo() {
+  async fetchApiByQuery() {
     this.setState(
       { loading: true },
       async () => {
-        const categoriesFromApi = await mlAPI.getProductsFromCategoryAndQuery(categoryId, searchParameter);
+        const segmentedItens = await mlAPI.getProductsFromCategoryAndQuery(this.state.category, this.state.searchElement);
         this.setState({
           loading: false,
-          arrayOfCategories: categoriesFromApi,
+          arrayOfItemByInputedText: [...segmentedItens.results],
         });
       });
-      console.log(parameterSearch)
   }
 
-  componentDidMount() {
-    this.fetchCategories();
-  }
-
-  componentDidUpdate() {
-    this.getApiInfo()
+  changeStateValue(event) {
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   render() {
-    const { arrayOfCategories, loading } = this.state;
+    const { arrayOfCategories, loading, arrayOfItemByInputedText } = this.state;
     const loadingElement = <span>Carregando...</span>;
-    
     return (
-      <div className="header-container">
-        <img className="logo" src={logo} alt="logo-react" />
-        <div className="search">
-            <div>
-              <input type="text"></input>
-              <div data-testid="home-initial-message">Digite algum termo de pesquisa ou escolha uma categoria.</div>
+      <div>
+        <header className="header-container">
+          <img className="logo" src={logo} alt="logo-react" />
+          <div className="search">
+            <div className="search-bar-content">
+              <img className="search-icon"
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEJTOvaCRMvUxPy8OR3W53CXP_eLOKV3QBaw&usqp=CAU"
+                alt="search icon" />
+              <input
+                data-testid="query-input"
+                className="search-bar"
+                type="text"
+                value={this.state.searchElement}
+                name="searchElement"
+                onChange={this.changeStateValue} />
             </div>
-          <div className="search-bar-content">
-            <img className="search-icon" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEJTOvaCRMvUxPy8OR3W53CXP_eLOKV3QBaw&usqp=CAU" alt="search icon" />
-            <input onChange={getInputString()} data-testid="query-input" className="search-bar" type="text" />
-          </div>
-          <div className="home-initial-message" data-testid="home-initial-message">
-            <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
-          </div>
-          <div>
-            {loading ? loadingElement : arrayOfCategories.map((categorie) => <Categories key={categorie.id} categorie={categorie} />)}
+            <div className="home-initial-message" data-testid="home-initial-message">
+              <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+            </div>
           </div>
           <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
+          <button data-testid="query-button" onClick={this.fetchApiByQuery} >Buscar</button>
+        </header >
+        <div>
+          {loading ? loadingElement : arrayOfCategories.map((categorie) => <Categories key={categorie.id} categorie={categorie} />)}
         </div>
-        <button data-testid="query-button" onClick={()=>{this.getApiInfo()}} >Buscar</button>
-      </div >
+        <div>
+          {arrayOfItemByInputedText.map((item) => <Card key={item.id} products={item} />)}
+        </div>
+      </div>
     );
   }
 }
-
-SearchBar.prototype = {}
 
 export default SearchBar;
