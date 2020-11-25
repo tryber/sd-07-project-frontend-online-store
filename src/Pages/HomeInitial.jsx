9b2cd1294caedfb7dad63c9a2538ce5498cde6c0
Raphael Caputo <faelelf2@gmail.com>
2020-11-25 16:23:41 -0300

    Consertando alguns issues

--- src/Pages/HomeInitial.jsx
+++ src/Pages/HomeInitial.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ShoppingCartButton from '../Components/ShoppingCartButton';
+import CategoryList from '../Components/CategoryList';
 
 class HomeInitial extends Component {
   render() {
     return (
       <div className="home-page">
+        <CategoryList />
         <span data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </span>
