285d24901115ff18f427bd4731860a02c928bc3d
Filipi Firmino <contatoic3web@gmail.com>
2020-11-24 21:05:23 -0300

    Adjusting the header and input CSS

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,11 +4,13 @@ class Header extends React.Component {
   render() {
     return (
       <div className="container-header">
-        <div className="logo">
-          <h1>Shopping</h1>
-        </div>
-        <div className="input-serch">
-          <input type="text" placeholder="Buscar" className="input" />
+        <div className="inner-container">
+          <div className="logo">
+            <h1>Shopping</h1>
+          </div>
+          <div className="input-serch">
+            <input type="text" placeholder="Buscar" className="input" />
+          </div>
         </div>
       </div>
     );
