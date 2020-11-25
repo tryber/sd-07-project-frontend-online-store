fa639e5286ba3bcc8403fa96002ed5d3a301f6a1
ericvini <ericcobain3@gmail.com>
2020-11-24 21:50:00 -0300

    Resolvendo issue

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,1 +1,14 @@
 import React, { Component } from 'react';
+
+class ProductList extends Component {
+  render() {
+    return (
+      <div>
+        <p>oi</p>
+      </div>
+
+    );
+  }
+}
+
+export default ProductList;
