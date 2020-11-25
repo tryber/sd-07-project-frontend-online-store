973b46f93a20b8c8477dd064b26843b7aa965767
vanderson-henrique <vhbenedito@gmail.com>
2020-11-25 14:19:25 -0300

    Requisito 6 - Filter categories

--- src/components/InitialScreen.jsx
+++ src/components/InitialScreen.jsx
@@ -15,20 +15,28 @@ class InitialScreen extends React.Component {
     this.fetchAPI = this.fetchAPI.bind(this);
     this.onInputSearchChange = this.onInputSearchChange.bind(this);
     this.buttonSearch = this.buttonSearch.bind(this);
+    this.onChangeCategorySelected = this.onChangeCategorySelected.bind(this);
     this.state = {
       searchInput: '',
       categories: [],
       products: [],
       message: 'Digite algum termo de pesquisa ou escolha uma categoria.',
+      categorySelected: '',
     };
   }
 
-  async componentDidMount() {
-    await this.fetchAPI();
+  componentDidMount() {
+    this.fetchAPI();
   }
 
   onInputSearchChange({ target }) {
     this.setState({ searchInput: target.value });
+  }
+
+  onChangeCategorySelected({ target }) {
+    this.setState({ categorySelected: target.id }, () => {
+      this.fetchProducts();
+    });
   }
 
   buttonSearch() {
@@ -36,9 +44,13 @@ class InitialScreen extends React.Component {
   }
 
   async fetchProducts() {
-    const { searchInput } = this.state;
+    const { searchInput, categorySelected } = this.state;
     const dataInputSearch = searchInput;
-    const getProducts = await api.getProductsFromCategoryAndQuery('query', dataInputSearch);
+    const datacategorySelected = categorySelected;
+    const getProducts = await api.getProductsFromCategoryAndQuery(
+      datacategorySelected,
+      dataInputSearch,
+    );
     this.setState({ products: getProducts.results });
   }
 
@@ -53,31 +65,34 @@ class InitialScreen extends React.Component {
   render() {
 
     const { message, searchInput, products, categories } = this.state;
-        
+
     return (
       <div className="container">
         <div className="side-bar">
-          <SideBar categories={ categories } />
+          <SideBar
+            categories={ categories }
+            onChangeCategorySelected={ this.onChangeCategorySelected }
+          />
         </div>
         <header className="container-initial-screen">
           <input
             data-testid="query-input"
             type="text"
             name="inputSearch"
-            value={searchInput}
-            onChange={this.onInputSearchChange}
+            value={ searchInput }
+            onChange={ this.onInputSearchChange }
           />
-          <button data-testid="query-button" type="submit" onClick={this.buttonSearch}>
+          <button data-testid="query-button" type="submit" onClick={ this.buttonSearch }>
             Buscar
           </button>
           <Link data-testid="shopping-cart-button" to="/kart">
-            <img src={KartImg} alt="Botão carrinho de compras" />
+            <img src={ KartImg } alt="Botão carrinho de compras" />
           </Link>
           <h1 data-testid="home-initial-message">{message}</h1>
         </header>
         <div>
           {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
+            <ProductCard key={ product.id } product={ product } />
           ))}
         </div>
       </div>
