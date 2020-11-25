7fbaf3863a98f5f5a31790186971c3bcf9b3204b
BrunaSepulveda <69821829+BrunaSepulveda@users.noreply.github.com>
2020-11-25 18:29:17 -0300

    Update ButtonShop.jsx

--- src/components/ButtonShop.jsx
+++ src/components/ButtonShop.jsx
@@ -5,14 +5,14 @@ import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
 class ButtonShop extends React.Component {
     render() {
-     return (
-        <IconButton color="primary" aria-label="add to shopping cart">
-          <Link to="/shoppage">
+      return (
+         <IconButton color="primary" aria-label="add to shopping cart">
+           <Link to="/shoppage">
               <AddShoppingCartIcon data-testid="shopping-cart-button"/>
-          </Link>
-        </IconButton>
-    );
-  }
+           </Link>
+         </IconButton>
+      );
+    }
 }
 
 export default ButtonShop;
