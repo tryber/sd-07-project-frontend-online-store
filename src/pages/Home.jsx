fb7631a2e74022fe05349ed6cba93dab3422d983
Rogerio-feitor <roger.feitor@gmail.com>
2020-11-25 19:18:47 -0300

    Resolução das funções de chamada para pesquisa do produto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import SearchBar from '../components/SearchBar';
+import '../App.css'
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searching: ''
+    }
+  }
+
+
+  
   render() {
     return (
-      <div>
+      <main>
         <header>
-          <SearchBar />
+          <div>
+            <h3>Aqui vai uma logo</h3></div>
+          <div>
+            <SearchBar />
+          </div>
+          <div></div>
         </header>
-      </div>
+      </main>
     );
   }
 }
---
+++ src/pages/ProductNotFound.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ProductNotFound extends Component {
+  render() {
+    return <h1>Produto não encontrada</h1>;
+  }
+}
+
+export default ProductNotFound;
