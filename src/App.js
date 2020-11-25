1a12b0f97605c4c5312233a395db5100d1c9425c
Renato Silva <renatosdev@gmail.com>
2020-11-24 22:34:12 -0300

    Cria componente de busca - com componets

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { ProductList, Home } from './components';
+import ProductList from './components/ProductList';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div>
       <Home />
-    <BrowserRouter>
-      <Route exact path="/" component={ ProductList } />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Route exact path="/" component={ProductList} />
+      </BrowserRouter>
     </div>
   );
 }
