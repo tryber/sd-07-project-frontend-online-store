7869058c4509bb850e1a583491960dfd9d9732ca
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-11-25 18:19:40 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 export async function getCategories() {
   const endpoint = 'https://api.mercadolibre.com/sites/MLB/categories';
-  console.log(endpoint);
   return fetch(endpoint)
     .then((res) => res.json())
     .catch((error) => console.log(error));
