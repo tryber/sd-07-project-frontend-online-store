55ae84ecf4b8ae508e721979e36f965aad90caa2
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-11-25 15:32:44 -0300

    criando componente Loading

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 class ProductDetails extends Component {
-    render() {
-        return (
-            <div>Página de detalhes do produto</div>
-        )
-    }
+  render() {
+    return (
+      <div>Página de detalhes do produto</div>
+    )
+  }
 }
 
 export default ProductDetails;
