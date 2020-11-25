971ce7d058f1b62148021e83509144f30856c59e
Marcos R. Silva <marcos.1998226@gmail.com>
2020-11-25 15:10:52 -0300

    Criar o campo de busca da tela principal

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Main from './pages/Main';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={Main}></Route>
+        <Route exact path="/" component={ Main } />
       </BrowserRouter>
     );
   }
 }
 
 export default App;
+
