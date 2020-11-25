15e3a7fe29182301cbb87be6ea0ba43e91048ae6
Paulo Henrique <ph.pereira.7@gmail.com>
2020-11-25 14:41:05 -0300

    Primeiro requisito com ajustes e com unix tentativa:2

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,13 @@
 export async function getCategories() {
-  const myObj = { method: 'GET' };
-  const myFetch = fetch('https://api.mercadolibre.com/sites/MLB/categories', myObj)
+  // const myObj = { method: 'GET' };
+  const myFetch = fetch('https://api.mercadolibre.com/sites/MLB/categories')
     .then((resolve) => resolve.json());
   return myFetch;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const myObj = { method: 'GET' };
-  const myFetch = fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`, myObj)
+  // const myObj = { method: 'GET' };
+  const myFetch = fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`)
     .then((resolve) => resolve.json())
   return myFetch;
 }
