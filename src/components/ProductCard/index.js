f1efec09ab391b7743ddff9bed34c5fd730bafee
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-11-26 17:01:57 -0300

    Ajustando Layout Req 07

---
+++ src/components/ProductCard/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './ProductCard.css';
+
+class ProductCard extends Component {
+  render() {
+    const { product } = this.props;
+    const { title, thumbnail, price } = product;
+    return (
+      <div
+        className="product-card"
+        data-testid="product"
+      >
+        <img
+          className="product-card-image"
+          src={ thumbnail }
+          alt="Product"
+        />
+        <div
+          className="product-card-info"
+        >
+          <h3
+            data-testid="product-detail-name"
+            className="product-card-title"
+          >
+            { title }
+          </h3>
+          <p>{ price }</p>
+          <Link
+            data-testid="product-detail-link"
+            to={ {
+              pathname: '/productdetail',
+              state: { product },
+            } }
+          >
+            Mais Detalhes
+          </Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    thumbnail: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default ProductCard;
--- src/pages/ProductDetail/index.js
+++ src/pages/ProductDetail/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './ProductDetail.css';
 
 class ProductDetail extends React.Component {
   render() {
     const { location: { state: { product } } } = this.props;
     return (
-      <div data-testid="product-detail-name">
-        { product.title }
+      <div className="detail-main-container">
+        <div className="detail-title-content" data-testid="product-detail-name">
+          <h2>{ product.title }</h2>
+        </div>
+        <div className="detail-info-container">
+          <img
+            className="detail-product-image"
+            src={ product.thumbnail }
+            alt="Product"
+          />
+          <div className="detail-summary-content">
+            <h3> Especificações Técnicas </h3>
+            <ul>
+              {product.attributes.map(({ id, name, value_name: value }) => (
+                <li key={ id }>{`${name}: ${value !== null ? value : 'N/D'}`}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
@@ -20,6 +38,11 @@ ProductDetail.propTypes = {
         title: PropTypes.string.isRequired,
         thumbnail: PropTypes.string.isRequired,
         price: PropTypes.number.isRequired,
+        attributes: PropTypes.arrayOf(PropTypes.shape({
+          id: PropTypes.string.isRequired,
+          name: PropTypes.string.isRequired,
+          value_name: PropTypes.string,
+        })).isRequired,
       }).isRequired,
     }).isRequired,
   }).isRequired,
