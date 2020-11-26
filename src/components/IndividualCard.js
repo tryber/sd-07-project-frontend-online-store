1a5ac06ea14750a0ed470cb4ccd59dff77df93a9
dennerpl <denner.souzapl@gmail.com>
2020-11-26 16:25:21 -0300

    adicionando link para detalhes do produto

--- src/components/IndividualCard.js
+++ src/components/IndividualCard.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class IndividualCard extends Component {
   render() {
-    const { key, price, image, title } = this.props;
+    const { key, price, image, title, id } = this.props;
     return (
       <div key={ key } data-testid="product">
-        <h4>{title}</h4>
+        <h4>{ title }</h4>
         <img src={ image } alt="titulo" />
-        <p>{price}</p>
-        <button type="button">Ver detalhes</button>
+        <p>{ price }</p>
+        <Link to={ `/product/${id}` }><button type="button">Ver detalhes</button></Link>
         <br />
         <button type="button">Adicionar ao carrinho</button>
       </div>
@@ -22,6 +23,7 @@ IndividualCard.propTypes = {
   price: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default IndividualCard;
