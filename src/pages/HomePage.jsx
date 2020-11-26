560dffc7322173cc3d6df9c04b53d7ff4e00e908
Mateus Leite Alves de Almeida <mateuslaalmeida@gmail.com>
2020-11-26 17:09:23 -0300

    Priority 2 - Requirement 6 - added category filters

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,21 +2,38 @@ import React, { Component } from 'react';
 import ProductList from '../components/ProductList/ProductList';
 import ShoppingCartButton from '../components/ShoppingCartButton/ShoppingCartButton';
 import CategoryList from '../components/CategoryList/CategoryList';
+import * as api from '../services/api';
 
 class HomePage extends Component {
-// constructor(props) {
-  //   super(props);
-  //   this.setState = {
-  //     category: 'xablau',
-  //   };
-  // }
+ constructor(props) {
+    super(props);
+    this.renderProducts = this.renderProducts.bind(this);
+    this.onChangeCategory = this.onChangeCategory.bind(this);
+    this.state = {
+      categoryIdSelected: '',
+      searchInput: '',
+      productList: [],
+    };
+  }
   
+  async renderProducts() {
+    const { searchInput, categoryIdSelected } = this.state;
+    const filteredProducts = await api.getProductsFromCategoryAndQuery(categoryIdSelected, searchInput);
+    this.setState({ productList: filteredProducts })
+  }
+
+  onChangeCategory({ target }) {
+    this.setState({
+      categoryIdSelected: target.id
+    }, () => this.renderProducts())
+  }
+
   render() {
+    const { categoryIdSelected} = this.state;
     return (
-      // const { category } = this.state;
       <div>
         <ShoppingCartButton />
-        <ProductList categoryId="MLB5672" />
+        <ProductList categoryId={ categoryIdSelected } />
         <CategoryList onChangeCategory={ this.onChangeCategory } />
       </div>
     );
