b505cfa47908269a3970caccf1481b5a0722a164
Lfocosi <luccafocosi@gmail.com>
2020-11-25 22:53:52 -0300

    add botão de sem categoria

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -5,21 +5,38 @@ class ProductDetails extends React.Component {
   constructor() {
     super()
     this.apiRequest = this.apiRequest.bind(this);
+    this.handleFitered = this.handleFitered.bind(this);
     this.state = {
-      item: {},
+      item: [],
+      itemRecived: {},
     }
   }
-  componentDidMount() { 
-    const param = this.props.match.params.title
-    this.apiRequest(param);
-  }
+
   async apiRequest(params) {
-    const productList = await api.getProductsFromCategoryAndQuery('', params) 
-    this.setState({ item: productList });
+    const productList = await api.getItemById(params);
+    const resultados = productList.results;
+    this.setState({ item: resultados });
+    this.handleFitered();
   }
+
+  handleFitered() {
+    const { id } = this.props.match.params;
+    const { item } = this.state;
+    const productRecived = item.filter(produc => produc.id === id);
+    this.setState({ itemRecived: productRecived[0] });
+  }
+
+  componentDidMount() { 
+    const { category } = this.props.match.params
+    this.apiRequest(category);
+  }
+
   render() {
+    const {id, title} = this.state.itemRecived;
     return (
-    <h1>Teste</h1>
+      <div>
+        <h1>{id} {title}</h1>
+      </div>
     )
   }
 }
