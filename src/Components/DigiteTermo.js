48bfd17b2607e993c4db82fe4b5b8eb689ec466c
Breno Alves <brenoalv3ss@gmail.com>
2020-11-26 14:05:56 -0300

    Refatoraçao, correçao do requiriment 5

--- src/Components/DigiteTermo.js
+++ src/Components/DigiteTermo.js
@@ -4,9 +4,9 @@ class DigiteTermo extends React.Component {
   render() {
     return (
       <div>
-          <p data-testid="home-initial-message">
-            Digite algum termo de pesquisa ou escolha uma categoria.
-          </p>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
       </div>
     );
   }
