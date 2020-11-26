b22f0beacf7fc4158b837979c3ab5b5a73d4138c
Pablo <pablopessanh@gmail>
2020-11-26 20:53:34 -0300

    lint fixes - 2

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,14 +3,16 @@ import React from 'react';
 class Cart extends React.Component {
   render() {
     const cartProduct = sessionStorage.getItem('item');
-    if (!cartProduct)
+    if (!cartProduct) {
       return <div data-testid="shopping-cart-empty-message">Seu carrinho está vazio</div>;
+    }
 
     return (
       <div>
         <span data-testid="shopping-cart-product-name">{cartProduct}</span>
         <p>
-          Quantidade:<span data-testid="shopping-cart-product-quantity">1</span>
+          Quantidade:
+          <span data-testid="shopping-cart-product-quantity">1</span>
         </p>
       </div>
     );
