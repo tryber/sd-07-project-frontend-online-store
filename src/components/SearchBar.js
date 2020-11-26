15fb0ad7b828ca3826392275ec9a18cd3870bcfc
Mauricio Homma <mhhomma@gmail.com>
2020-11-26 00:05:12 -0300

    test evaluator

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import CategoryList from './CategoryList';
+
 class SearchBar extends React.Component {
   render() {
     return (
-      <div>Barra de busca</div>
+      <div>
+        Barra de busca
+        <CategoryList />
+      </div>
     );
   }
 }
