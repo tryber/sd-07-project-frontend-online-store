a4ee89a0684b35ab047598ae569f37aef01080d3
afonsofn <afonsoafn@gmail.com>
2020-11-26 16:14:00 -0300

    Atualizando o card para linkar com detalhes

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
 import '../App.css';
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 class ProductCard extends React.Component {
   render() {
-    const { title, price, thumbnail } = this.props.item
+    const { item } = this.props
+    const { id ,title, price, thumbnail } = item
     return (
-      <section data-testid="product">
-        <img alt="Sell Product" className="card-image" src={thumbnail} />
-        <div className="info">
-          <h4>{title}</h4>
-          <h5>{price}</h5>
-        </div>
-        <hr />
-      </section>
+      <div>
+        <section data-testid="product" >
+          <img alt="Sell Product" className="card-image" src={thumbnail} />
+          <div className="info">
+            <h4>{title}</h4>
+            <h5>{price}</h5>
+          </div>
+          <Link
+            data-testid="product-detail-link"
+            to={ {
+              pathname: `/pages/ProductDetails/${id}`,
+              state: { item },
+            } }
+          >
+            Mais Detalhes
+          </Link>
+          <hr />
+        </section>
+      </div>
     );
   }
 }
 
-// ProductCard.propTypes = {
-//   movie: PropTypes.shape({
-//     id: PropTypes.number,
-//     title: PropTypes.string,
-//     storyline: PropTypes.string,
-//     imagePath: PropTypes.string,
-//   }).isRequired,
-// };
+ProductCard.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    price: PropTypes.string,
+    thumbnail: PropTypes.string,
+  }).isRequired,
+};
 
 export default ProductCard;
