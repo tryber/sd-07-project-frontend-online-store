31c8beed405897fdd275bd0eaa02ca28f1dc57c9
Emerson Moonlit <emoonlit@pop-os.localdomain>
2020-11-25 22:07:08 -0300

    Create component Loading

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       < GlobalStyle />
-      < cp.Header />
+    
 
     </div>
   );
--- src/style/global.js
+++ src/style/global.js
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
-export default createGlobalStyle`
+
+export default createGlobalStyle`    
     * {
         margin: 0;
         padding: 0;
@@ -12,6 +13,12 @@ export default createGlobalStyle`
         font-family: Arial, Helvetica, sans-serif;
         font-size: 16px;
         background-color: #f5f5f5;
+    }
+
+    @keyframes is-rotating {
+        to {
+            transform: rotate(2turn);
+        }
     }
 
 `;
