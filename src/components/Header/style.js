83f2f5ec87f1e1d24e6043e34482ea6bb099ce4c
Emerson Moonlit <emoonlit@pop-os.localdomain>
2020-11-25 21:18:46 -0300

    Create Header and GlobalCss

---
+++ src/components/Header/style.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+    background: linear-gradient(to right, #1EFF9080, #1E90FF, #08264298);
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .ctn-center {
+        border: 1px solid black;
+        width: 85%;
+        display: flex;
+        padding: 16px;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+`;
