e24fc4588a23a62355a84a89a3416b587b3654ae
Mauricio Hamaji Homma <43273469+mhamaji@users.noreply.github.com>
2020-11-25 23:46:18 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';	
 
-import CategoryList from './components/CategoryList';
+
+import Products from './pages/Products';	
+import ShoppingCart from './pages/ShoppingCart';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <CategoryList />
+      <BrowserRouter>	
+        <Switch>	
+          <Route path="/shopping-cart" component={ ShoppingCart } />	
+          <Route exact path="/" component={ Products } />	
+        </Switch>	
+      </BrowserRouter>
     </div>
   );
 }
