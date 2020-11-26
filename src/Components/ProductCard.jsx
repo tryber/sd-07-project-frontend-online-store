c7089787306030a0684c90f32ab49f76080b2052
moraisgabri <gabriel8azevedo@gmail.com>
2020-11-26 17:49:56 -0300

    finalmente, com muita satisfação requisito 7

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class ProductCard extends React.Component {
@@ -6,11 +7,19 @@ class ProductCard extends React.Component {
     const { product } = this.props;
     const { title, thumbnail, price } = product;
     return (
-      <div>
-        <div>{ title }</div>
-        <div>{ price }</div>
-        <img src={ thumbnail } alt="exemplar do produto" />
-      </div>
+      <Link
+        to={ {
+          pathname: '/product',
+          state: { product },
+        } }
+        data-testid="product-detail-link"
+      >
+        <div>
+          <h1>{ title }</h1>
+          <div>{ price }</div>
+          <img src={ thumbnail } alt="exemplar do produto" />
+        </div>
+      </Link>
     );
   }
 }
