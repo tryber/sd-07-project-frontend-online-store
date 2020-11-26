e1402407f36d631e3362e8143b0ddb72424f97a6
Marcos R. Silva <marcos.1998226@gmail.com>
2020-11-26 18:31:37 -0300

    Estilo CSS

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import chart from '../icon/chart.png';
-import lupa from '../icon/lupa.png';
 import '../App.css';
 import Categories from '../components/Categories';
 import ListCard from '../components/ListCard';
@@ -9,25 +8,23 @@ import * as api from '../services/api';
 
 class Main extends React.Component {
   constructor(props) {
-    super()
+    super();
     this.handleValue = this.handleValue.bind(this);
     this.handleCatChange = this.handleCatChange.bind(this);
     this.handleApiRequest = this.handleApiRequest.bind(this);
     this.state = {
-      search:'',
-      catID:'',
-      message:'Digite algum termo de pesquisa ou escolha uma categoria.',
-    }
+      search: '',
+      catID: '',
+      message: 'Digite algum termo de pesquisa ou escolha uma categoria.',
+    };
   }
   handleApiRequest(event) {
-
     const { search, catID } = this.state;
-    if(search === '' && catID === '') {
+    if (search === '' && catID === '') {
       this.setState({ message: 'Nenhum produto encontrado' });
     }
-    console.log(this.state.message)
+    console.log(this.state.message);
     console.log(api.getProductsFromCategoryAndQuery(catID, search));
-    
   }
   handleCatChange(event) {
     const selectedID = event.target.id;
@@ -42,23 +39,28 @@ class Main extends React.Component {
   render() {
     const { search } = this.state;
     return (
-      <section className="main-container">
-        <div className="container">
-          <img className="lupa" src={ lupa } alt="lupa" />
-          <input data-testid="query-input" onChange={this.handleValue} className="searchInput" type="search" />
-          <button data-testid="query-button" onClick={this.handlhandleApiRequesteValue} >Buscar</button>
-          <h3 data-testid="home-initial-message">
-            {this.state.message}
-          </h3>
-          <Link to="/shoppingCart" data-testid="shopping-cart-button" >
-            <img className="chartImg" src={ chart } alt="carrinho-de-compras" />
+      <div className="container">
+
+        <div className="input-1">
+          <input data-testid="query-input" onChange={this.handleValue} className="searchInput" type="search"/>
+          <h3 data-testid="home-initial-message">{this.state.message}</h3>
+        </div>
+
+        <div className="linkToCart-2">
+          <Link to="/shoppingCart" data-testid="shopping-cart-button">
+            <img className="chartImg" src={chart} alt="carrinho-de-compras" />
           </Link>
-          <ListCard value={ search } />
+          <ListCard value={search} />
         </div>
-        <div className="container">
-          <Categories handleCatChange={this.handleCatChange}/>
+
+        <div class="categories-3">
+          <Categories handleCatChange={this.handleCatChange} />
         </div>
-      </section>
+
+        <div className="buttonFetch-4">
+          <button className="buttonFetch" data-testid="query-button" onClick={this.handlhandleApiRequesteValue}> Buscar </button>
+          </div>
+      </div>
     );
   }
 }
