b46746ca092770a146b4473c6055107b2b9bba86
Eduardo Souza <dado.eletrons@gmail.com>
2020-11-26 05:53:15 -0300

    update main group 13

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,8 @@ export { default as CardProduct } from './CardProduct';
 export { default as Icon } from './Icons';
 export { default as Loading } from './Loading';
 
+export { default as Teste } from './Teste';
+
 // Inputs
 export { default as Button } from './forms/Button';
 export { default as Input } from './forms/Input';
