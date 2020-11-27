44266f935c93200be6d5226e386348cf0ff6e68e
Raphael Caputo <faelelf2@gmail.com>
2020-11-27 15:14:42 -0300

    COMMIT FECHADO!

--- src/Components/ReturnButton.jsx
+++ src/Components/ReturnButton.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './ReturnButton.css';
 
-
 class ReturnButton extends Component {
   render() {
     const { path } = this.props;
--- src/Components/ShoppingCartButton.jsx
+++ src/Components/ShoppingCartButton.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './ShoppingCartButton.css';
 
-
 class ShoppingCartButton extends React.Component {
   render() {
     return (
