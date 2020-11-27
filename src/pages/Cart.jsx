bd2be5ecf83733a5ee2daa4ff998e5786a776b95
kramersilva <kramerscs@gmail.com>
2020-11-26 23:25:29 -0300

    eslintt

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,25 +2,29 @@ import React from 'react';
 
 class Cart extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       quantity: 1,
-    }
+    };
     this.increase = this.increase.bind(this);
     this.decrease = this.decrease.bind(this);
   }
 
   increase() {
-    this.setState({ quantity: this.state.quantity + 1});
+    this.setState((previous) => {
+      quantity: previous + 1,
+    });
   }
 
   decrease() {
-    const previous = this.state.quantity;
-    const result = previous - 1;
-    result < 0 ? this.setState({ quantity: 0 }) : this.setState({ quantity: result });
+    const reset = 0
+    const { quantity } = this.state;
+    const result = quantity - 1;
+    result < 0 ? this.setState({ quantity: reset }) : this.setState({ quantity: result });
   }
+
   render() {
-    const { quantity } = this.state
+    const { quantity } = this.state;
     const cartProduct = sessionStorage.getItem('item');
     if (!cartProduct) {
       return <div data-testid="shopping-cart-empty-message">Seu carrinho está vazio</div>;
@@ -31,9 +35,9 @@ class Cart extends React.Component {
         <span data-testid="shopping-cart-product-name">{cartProduct}</span>
         <p>
           Quantidade:
-    <span data-testid="shopping-cart-product-quantity">{quantity}</span>
-          <button data-testid="product-increase-quantity" onClick={this.increase}><i className="fas fa-plus"></i></button>
-          <button data-testid="product-decrease-quantity"onClick={this.decrease}><i className="fas fa-minus"></i></button>
+        <span data-testid="shopping-cart-product-quantity">{quantity}</span>
+        <button data-testid="product-increase-quantity" onClick={this.increase}><i className="fas fa-plus"></i></button>
+        <button data-testid="product-decrease-quantity" onClick={this.decrease}><i className="fas fa-minus"></i></button>
         </p>
       </div>
     );
