d84213ba1d3700af045fe41797084ba771b29946
danielmlduarte <danielduarte.tryber@gmail.com>
2020-11-26 23:02:25 -0300

    Requerimento 10 - Adicionar, remover quantidade e excluir item do carrinho

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class CartItem extends Component {
+  handleCartItemIncrease(product) {
+    const { addItem } = this.props;
+    addItem(product);
+  }
+
+  handleCartItemRemove(product) {
+    const { removeItem } = this.props;
+    removeItem(product);
+  }
+
+  handleCartItemDecrease(product) {
+    const { decreaseItem } = this.props;
+    decreaseItem(product);
+  }
+
   render() {
-    const { data: { title, thumbnail, price, cartQuantity } } = this.props;
+    const { data } = this.props;
+    const { title, thumbnail, price, cartQuantity } = data;
     return (
       <article>
         <header>
@@ -16,6 +32,26 @@ class CartItem extends Component {
           <p>{`R$ ${price}`}</p>
           <p data-testid="shopping-cart-product-quantity">{ cartQuantity }</p>
         </footer>
+        <button
+          type="button"
+          data-testid="product-increase-quantity"
+          onClick={ () => this.handleCartItemIncrease(data) }
+        >
+          +
+        </button>
+        <button
+          type="button"
+          data-testid="product-decrease-quantity"
+          onClick={ () => this.handleCartItemDecrease(data) }
+        >
+          -
+        </button>
+        <button
+          type="button"
+          onClick={ () => this.handleCartItemRemove(data) }
+        >
+          X
+        </button>
       </article>
     );
   }
@@ -29,6 +65,9 @@ CartItem.propTypes = {
     id: PropTypes.string.isRequired,
     cartQuantity: PropTypes.number.isRequired,
   }).isRequired,
+  addItem: PropTypes.func.isRequired,
+  removeItem: PropTypes.func.isRequired,
+  decreaseItem: PropTypes.func.isRequired,
 };
 
 export default CartItem;
--- src/pages/ShoppingCart/index.js
+++ src/pages/ShoppingCart/index.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 
-import { getCartList } from '../../services/cartApi';
+import * as cartApi from '../../services/cartApi';
 import CartItem from '../../components/CartItem';
 
 class ShoppingCart extends Component {
   constructor() {
     super();
+    this.handleCartItemIncrease = this.handleCartItemIncrease.bind(this);
+    this.handleCartItemRemove = this.handleCartItemRemove.bind(this);
+    this.handleCartItemDecrease = this.handleCartItemDecrease.bind(this);
     this.state = {
       cart: [],
+      cartTotal: 0,
     };
   }
 
@@ -15,18 +19,56 @@ class ShoppingCart extends Component {
     this.updateCart();
   }
 
+  getTotalValue(total, product) {
+    return total + product.price * product.cartQuantity;
+  }
+
   updateCart() {
-    this.setState({ cart: getCartList() });
+    const initialValue = 0;
+    const cart = cartApi.getCartList();
+    const cartTotal = cart.reduce(this.getTotalValue, initialValue);
+    this.setState({
+      cart,
+      cartTotal,
+    });
+  }
+
+  handleCartItemIncrease(product) {
+    cartApi.addToCart(product);
+    this.updateCart();
+  }
+
+  handleCartItemRemove(product) {
+    cartApi.removeFromCart(product);
+    this.updateCart();
+  }
+
+  handleCartItemDecrease(product) {
+    cartApi.decreaseToCart(product);
+    this.updateCart();
   }
 
   render() {
-    const { cart } = this.state;
+    const { cart, cartTotal } = this.state;
     return (
       <div>
         <h4 data-testid="shopping-cart-empty-message">
           {!cart.length && <p>Seu carrinho está vazio.</p> }
-          {cart.map((cartItem) => <CartItem key={ cartItem.id } data={ cartItem } />)}
+          {cart.map((cartItem) => (
+            <CartItem
+              key={ cartItem.id }
+              data={ cartItem }
+              addItem={ this.handleCartItemIncrease }
+              removeItem={ this.handleCartItemRemove }
+              decreaseItem={ this.handleCartItemDecrease }
+            />
+          ))}
         </h4>
+        <footer>
+          <h5>
+            {`Valor total R$ ${cartTotal}`}
+          </h5>
+        </footer>
       </div>
     );
   }
