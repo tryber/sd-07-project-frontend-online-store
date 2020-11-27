d1970c849383304f07a0c1373d1d505449f54d75
Berilo Wanderley <wberilo@gmail.com>
2020-11-26 23:12:08 -0300

    Update src/services/api.js

--- src/services/api.js
+++ src/services/api.js
@@ -11,7 +11,6 @@ export async function getCategories() {
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  console.log('getProductsFromCategoryAndQuery was called')
   try {
     const response = await fetch(
       `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`,
