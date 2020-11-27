0bfc200e7a9461fc5d95255bf6887b184b86a1cf
josielvs <josielvs@gmail.com>
2020-11-27 14:51:09 -0300

    Requisito 12

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import { Kart, ProductDetail } from './components';
+import { Kart, ProductDetail, PayProductList } from './components';
 import InitialScreen from './components/InitialScreen';
 import './App.css';
 
@@ -12,6 +12,7 @@ class App extends React.Component {
           <Route exact path="/" component={ InitialScreen } />
           <Route exact path="/kart" component={ Kart } />
           <Route exact path="/detail/:id" component={ ProductDetail } />
+          <Route exact path="/pay" component={ PayProductList } />
         </Switch>
       </BrowserRouter>
     );
