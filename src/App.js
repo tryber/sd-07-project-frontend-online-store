ca9d7186b68debf53915c36e80e5f1a6370ecf02
gabrieldeori <deori.pessoal@gmail.com>
2020-11-27 17:00:00 -0300

    Insert Route to checkout on App

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ShoppingCart from './components/ShoppingCart';
 import ProductList from './components/ProductList';
 import ProductDetails from './components/ProductDetails';
+import Checkout from './components/Checkout';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/checkout" component= { Checkout }/>
         <Route path="/carrinho" component={ ShoppingCart } />
         <Route path="/product" component={ ProductDetails } />
         <Route exact path="/" component={ ProductList } />
