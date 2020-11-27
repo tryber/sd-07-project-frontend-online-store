30fc5b033542615cd74e4ac0699782fa2f01d02c
Ricardo Heck Alves <ricardoheckalves@icloud.com>
2020-11-27 10:42:18 -0300

    Adicionado lógica para recupurar e exibir lista de porodutos adicionados ao carrinho.

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react';
+import ShowCartItems from '../components/ShowCartItems';
 
 class ShoppingCart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cartItems: [],
+    };
+    this.retrieveItemsFromStorage = this.retrieveItemsFromStorage.bind(this);
+  }
+
+  componentDidMount() {
+    this.retrieveItemsFromStorage();
+  }
+
+  retrieveItemsFromStorage() {
+    const itemsInStorage = localStorage.getItem('cartItems');
+    const retrievedItems = JSON.parse(itemsInStorage);
+    console.log(retrievedItems);
+    this.setState({
+      cartItems: retrievedItems,
+    });
+  }
+
   render() {
+    const { cartItems } = this.state;
+    if (!cartItems || !cartItems.length) {
+      return (
+        <div data-testid="shopping-cart-empty-message">
+          Seu carrinho está vazio
+        </div>
+      );
+    }
+
     return (
-      <div data-testid="shopping-cart-empty-message">
-        Seu carrinho está vazio
+      <div>
+        {cartItems.map((item) => (
+          <ShowCartItems
+            key={ item.id }
+            item={ {
+              title: item.title,
+              price: item.price,
+              quantity: '1',
+            } }
+          />
+        ))}
       </div>
     );
   }
