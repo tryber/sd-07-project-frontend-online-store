38e96b396e53fb5fc6671e6ecbe2ab1545d5d9c4
vini-lima <vini20.henrique@gmail.com>
2020-11-26 22:23:15 -0300

    Arrumando o lint

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as InitialScreen } from './InitialScreen';
 export { default as SideBar } from './SideBar';
 export { default as Kart } from './Kart';
 export { default as ProductCard } from './ProductCard';
