fa45e17721998446d3235009da92f14c3bffa8d4
Camila Valentim <camilaifpb@hotmail.com>
2020-11-27 09:33:01 -0300

    requisito 5

--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,74 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Button from './components/Button';
+import { getProductsFromQuery } from  '../src/services/api';
+import ProductCard from '../src/components/ProductCard';
+
+
 class ProductsList extends Component {
+  constructor() {
+    super();
+    this.state = {
+        status: false,
+        products: [],
+        demand: '',
+    }
+
+    this.fecthProducts = this.fecthProducts.bind(this);
+    this.change = this.change.bind(this);
+}
+
+async fecthProducts() {
+    const { demand } = this.state
+    const resultRequest = await getProductsFromQuery(demand);
+    console.log(resultRequest);
+    if(resultRequest === "") return <div>Nenhum produto foi encontrado</div>
+    this.setState({
+        status: true,
+        products: resultRequest.results,
+    });
+    
+} 
+
+change({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+}
+
+
+
+
   render() {
-    const value = '';
+    const { status, products } = this.state;
     return (
       <div>
-        <form>
+        {(status) ? <ProductCard  products={products} /> : false}
+
+
+        
           <div>
             <label htmlFor="inputHome">
               <div>
                 <input 
                   data-testid="query-input"
-                  value={ value }
+                  name="demand"
                   className="inputHome"
                   type="text"
-                  onChange={ onChange }
+                  onChange={this.change}
+                  
                 />
-                <button data-testid="query-button">PESQUISAR</button>
+                <button data-testid="query-button" onClick={this.fecthProducts}>PESQUISAR</button>
                 <span data-testid="home-initial-message">
                   Digite algum termo de pesquisa ou escolha uma categoria.
                 </span>
               </div>
             </label>
-            <div>
-              <Button />
-            </div>
+            
           </div>
-        </form>
+    
       </div>
     );
   }
 }
-Button.propTypes = {
-  onClhange: PropTypes.func.isRequired,
-};
+
+
 export default ProductsList;
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react';
 
 class ProductCard extends Component {
     render() {
+        const { products } = this.props
         return (
-
-        )
+            <div>
+                {products.map((product) => {
+                   const { id, title, thumbnail, price } = product
+                   return (
+                    <div key={ id } data-testid="product">
+                        <h2>{ title }</h2>
+                        <img src={ thumbnail } alt="products" />
+                        <p>{ price }</p>
+                    </div>
+                    
+                   );
+                })}
+            </div>
+        );
     }
 }
 
