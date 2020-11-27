1c98b429a25bdcde5d68370ca0297fdd649a9987
Luise Rios <luisemiranda@gmail.com>
2020-11-27 16:58:25 -0300

    requisito 8 - adicionar produto ao carrinho

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -3,23 +3,47 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class ProductCard extends React.Component {
+  constructor() {
+    super();
+
+    this.addItemToCart = this.addItemToCart.bind(this);
+  }
+
+  addItemToCart(product) {
+    if (localStorage.getItem(product)) {
+      const quantity = JSON.parse(localStorage.getItem(product)) + 1;
+      localStorage.setItem(product, quantity);
+    } else {
+      localStorage.setItem(product, 1);
+    }
+  }
+
   render() {
     const { product } = this.props;
     const { title, thumbnail, price } = product;
     return (
-      <Link
-        to={ {
-          pathname: '/product',
-          state: { product },
-        } }
-        data-testid="product-detail-link"
-      >
-        <div>
-          <h1>{ title }</h1>
-          <div>{ price }</div>
-          <img src={ thumbnail } alt="exemplar do produto" />
-        </div>
-      </Link>
+      <section>
+        <Link
+          to={ {
+            pathname: '/product',
+            state: { product },
+          } }
+          data-testid="product-detail-link"
+        >
+          <div>
+            <h1>{ title }</h1>
+            <div>{ price }</div>
+            <img src={ thumbnail } alt="exemplar do produto" />
+          </div>
+        </Link>
+        <button
+          data-testid="product-add-to-cart"
+          type="button"
+          onClick={ () => this.addItemToCart(title) }
+        >
+          Adicionar ao Carrinho
+        </button>
+      </section>
     );
   }
 }
--- src/Pages/ShoppingCart.jsx
+++ src/Pages/ShoppingCart.jsx
@@ -6,10 +6,18 @@ class ShoppingCart extends React.Component {
     return (
       <div>
         <ReturnButton path="/" />
-        <div data-testid="shopping-cart-empty-message">
-          Seu carrinho está vazio
-        </div>
+        {localStorage.length === 0
+          ? <div data-testid="shopping-cart-empty-message">
+            Seu carrinho está vazio
+          </div>
+          : Object.keys(localStorage).map((key) => (
+            <div key={ key }>
+              <p data-testid="shopping-cart-product-name">{key}</p>
+              <p data-testid="shopping-cart-product-quantity">{localStorage.getItem(key)}</p>
+            </div>
+          ))}
       </div>
+
     );
   }
 }
