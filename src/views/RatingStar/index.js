4ecefa690afca394f8351d8104e649defec8ed07
Eduardo Souza <dado.eletrons@gmail.com>
2020-11-27 04:22:53 -0300

    [group 13] create view rating star

---
+++ src/views/RatingStar/index.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import * as css from './style';
+import * as icon from '../../components/Icons';
+
+export class RatingStar extends Component {
+  constructor(props) {
+    super(props);
+    const { rating } = this.props;
+    this.state = {
+      rating: rating,
+      hoverValue: null,
+    };
+  }
+
+  async eventHandleClick(name, value) {
+    const { onClick } = this.props;
+    await this.setState({ [name]: value });
+    onClick(this.state.rating);
+  }
+
+  setColorModeView(numberStar) {
+    const { rating, hoverValue } = this.state;
+    return numberStar <= (hoverValue || rating) ? '#ffc107' : '#aaa';
+  }
+
+  renderModeViewCard() {
+    const { mode } = this.props;
+
+    return [...Array(5)].map((_star, index) => {
+      const numberStar = index + 1;
+      return (
+        <div key={index}>
+          <icon.Star modeview={mode} setcolor={this.setColorModeView(numberStar)} />
+        </div>
+      );
+    });
+  }
+
+  renderModeViewInput() {
+    const { mode } = this.props;
+
+    return [...Array(5)].map((_star, index) => {
+      const numberStar = index + 1;
+      return (
+        <label key={index}>
+          <input
+            name="rating"
+            className="inputRadio"
+            type="radio"
+            onClick={() => this.eventHandleClick('rating', numberStar)}
+          />
+          <icon.Star
+            modeview={mode}
+            setcolor={this.setColorModeView(numberStar)}
+            onMouseEnter={() => this.eventHandleClick('hoverValue', numberStar)}
+            onMouseLeave={() => this.eventHandleClick('hoverValue', null)}
+          />
+        </label>
+      );
+    });
+  }
+
+  render() {
+    const { mode } = this.props;
+    return (
+      <css.Ctn>
+        {mode === 'input' && this.renderModeViewInput()}
+        {mode === 'card' && this.renderModeViewCard()}
+      </css.Ctn>
+    );
+  }
+}
+
+export default RatingStar;
