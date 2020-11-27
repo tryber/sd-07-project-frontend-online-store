3a7bf6fdb3fbd033628a9454710dcfb236a460b9
vitorAmorims <vtamorims@gmail.com>
2020-11-26 22:27:16 -0300

    Requisito10

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,34 +1,37 @@
-import React, { Component } from "react";
-import CartItem from "../components/CartItem"
+import React, { Component } from 'react';
+import CartItem from "../components/CartItem";
 
 class ShoppingCart extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      products: JSON.parse(localStorage.getItem('cart')),
+      products: JSON.parse(localStorage.getItem("cart")),
     };
   }
-  
+
   render() {
     const { products } = this.state;
-    console.log(products);
     if (products === null)
       return (
         <span data-testid="shopping-cart-empty-message">
           Seu carrinho está vazio
         </span>
       );
-    return (
-      <div>
-          {products.map((product) => <CartItem
-            key={product.id}
-            title={product.title}
-            price={product.price}
-            image={product.imagePath}
-            number={product.number} />
-          )}
+    return products.map((product) => {
+      return (
+        <div key={product.id}>
+          {
+            <CartItem
+              id={product.id}
+              title={product.title}
+              price={product.price}
+              image={product.imagePath}
+              number={product.number}
+            />
+          }
         </div>
-      )
+      );
+    });
   }
 }
 
