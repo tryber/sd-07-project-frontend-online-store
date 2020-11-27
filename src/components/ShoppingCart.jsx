0f3852a50f159a4d2391baebeb68ee6dd6337a0e
Israel Menon <zezomenon@gmail.com>
2020-11-27 20:32:39 -0300

    Adicionado funcionalidade no componente ShoppingCart para mostrar os dados salvos no localstorage

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 
 class ShoppingCart extends Component {
+  constructor(props) {
+    super(props);
+    this.restoreState = this.restoreState.bind(this);
+    this.state = {
+      productsTeste: [],
+    };
+  }
+
+  componentDidMount() {
+    this.restoreState();
+    console.log('1');
+  }
+
+  async restoreState() {
+    const products = await JSON.parse(localStorage.getItem('itemsCart'));
+    this.setState({ productsTeste: products });
+  }
+
   render() {
+    const { productsTeste } = this.state;
     return (
       <div>
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        {productsTeste ? productsTeste.map((item) => (
+          <div key={ item.title }>
+            <p data-testid="shopping-cart-product-name">{ item.title }</p>
+            <p>{ item.price }</p>
+            <p data-testid="shopping-cart-product-quantity">{ item.qtd }</p>
+          </div>
+        )) : <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>}
+        {/* <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p> */}
       </div>
     );
   }
