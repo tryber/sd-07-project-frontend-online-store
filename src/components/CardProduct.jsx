0615d72e9de18194f2264f69573eef9edc3e81b3
Lets <letsferreiratx@gmail.com>
2020-11-26 21:37:25 -0300

    tentando colcar itens no localStorage

--- src/components/CardProduct.jsx
+++ src/components/CardProduct.jsx
@@ -4,21 +4,41 @@ import '../App.css';
 import { Link } from 'react-router-dom';
 
 class CardProduct extends React.Component {
+  constructor() {
+    super();
+    this.addToLocalStorage = this.addToLocalStorage.bind(this);
+  }
+
+  addToLocalStorage() {
+    const { item } = this.props;
+    const { id } = item;
+    localStorage.setItem(id, JSON.stringify(item));
+  }
+
   render() {
     const { item } = this.props;
     const { title, price, thumbnail } = item;
     return (
-      <Link
-        to={ { pathname: `/${title}`, product: { item } } }
-        data-testid="product"
-        className="item-card"
-      >
-        <div data-testid="product-detail-link">
-          <img src={ thumbnail } alt={ title } className="card-image" />
-          <h4>{ title }</h4>
-          <p>{ `R$ ${price}` }</p>
-        </div>
-      </Link>
+      <div>
+        <Link
+          to={ { pathname: `/${title}`, product: { item } } }
+          data-testid="product"
+          className="item-card"
+        >
+          <div data-testid="product-detail-link">
+            <img src={ thumbnail } alt={ title } className="card-image" />
+            <h4>{title}</h4>
+            <p>{`R$ ${price}`}</p>
+          </div>
+        </Link>
+        <button
+          type="button"
+          data-testid="product-add-to-cart"
+          onClick={ this.addToLocalStorage }
+        >
+          Adicionar ao carrinho
+        </button>
+      </div>
     );
   }
 }
@@ -26,6 +46,7 @@ class CardProduct extends React.Component {
 CardProduct.propTypes = {
   item: PropTypes.shape({
     title: PropTypes.string,
+    id: PropTypes.string,
     price: PropTypes.number,
     thumbnail: PropTypes.string,
   }).isRequired,
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,11 +2,24 @@ import React from 'react';
 import '../App.css';
 
 class Cart extends React.Component {
+  constructor() {
+    super();
+    this.loadList = this.loadList.bind(this);
+  }
+
+  loadList() {
+    const values = Object.values(localStorage);
+    values.forEach((value) => {
+      const { id, title, price, } = JSON.parse(value);
+    });
+  }
+
   render() {
     return (
       <p
         data-testid="shopping-cart-empty-message"
         className="empty-cart"
+        onLoad={ this.loadList() }
       >
         Seu carrinho está vazio
       </p>
