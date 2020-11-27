ab2a35251d84ca99bf686179b7472bc3e736016c
Guilherme Almeida <almeidaguilherme204@gmail.com>
2020-11-27 18:57:57 -0300

    Lint

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import * as api from '../services/api';
 import Loading from '../components/Loading/Loading';
 
+
 class ProductDetails extends Component {
   constructor(props) {
     super(props);
@@ -49,6 +50,7 @@ class ProductDetails extends Component {
               </li>
             ))}
           </ul>
+         
         </div>
       </div>
     );
