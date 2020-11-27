d977f158f36121a9bc35963176dfc7588fb51c21
gabrieldeori <deori.pessoal@gmail.com>
2020-11-27 15:44:12 -0300

    Commit para fazer o pull

---
+++ src/components/Checkout.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import BuyerInformation from './checkout/BuyerInformation'
+import PaymentMethod from './checkout/PaymentMethod'
+import ReviewProducts from './checkout/ReviewProducts'
+import * as utils from '../services/utils'
+
+class Checkout extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    }
+    this.getProductsFromlocalStorage = this.getProductsFromlocalStorage.bind(this);
+  }
+
+  componentWillMount() {
+    localStorage.setItem()
+  }
+
+  getProductsFromlocalStorage() {
+    const productsFromCart = utils.getFromLocalStorage()
+    this.setState({
+      products: productsFromCart,
+    });
+  }
+
+  render() {
+    { products } = this.state;
+    return (
+      <div>
+        <ReviewProducts products={products} />
+        <BuyerInformation />
+        <PaymentMethod />
+        <button>Comprar</button>
+      </div>
+    );
+  }
+}
+
+export default Checkout;
---
+++ src/components/checkout/ReviewProducts.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+class ReviewProducts extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+      totalPrice: 0,
+    };
+    this.renderProducts = this.renderProducts.bind(this);
+  }
+
+  componentDidMount() {
+    { products, totalPrice } = this.props;
+  }
+
+  renderProducts() {
+    { products } = this.state;
+    products.map((product) => {
+      <div>
+        <img src={product.img} alt="" />
+        <p>{product.title}</p>
+        <p>{product.price}</p>
+      </div>
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Revise Seus produtos</h2>
+        { renderProducts()}
+        <p>Total: R${totalPrice}</p>
+      </div>
+    );
+  }
+}
+
+export default ReviewProducts;
