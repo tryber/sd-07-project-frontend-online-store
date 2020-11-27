fced486419a3b0e330a48f93a47a53c402b0f80b
berchon <berchon@gmail.com>
2020-11-27 01:25:59 -0300

    Requisito 3 e também algumas alterações no requisito 4 v1.7

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,27 +3,25 @@ import PropTypes from 'prop-types';
 
 class Category extends React.Component {
   mapCategories(categories) {
-    return (categories.map(({ id, name }) => (<p key={ id }>
-      <label htmlFor={ id }>
-        <input
-          type="radio"
-          data-testid="category"
-          id={ id }
-          name="category"
-          value={ id }
-        />
-        {name}
-      </label>
-    </p>)));
+    return categories.map(({ id, name }) => (
+      <p key={ id }>
+        <label htmlFor={ id }>
+          <input
+            type="radio"
+            data-testid="category"
+            id={ id }
+            name="category"
+            value={ id }
+          />
+          { name }
+        </label>
+      </p>
+    ));
   }
 
   render() {
     const { categories } = this.props;
-    return (
-      <aside>
-        {this.mapCategories(categories)}
-      </aside>
-    );
+    return <aside>{this.mapCategories(categories)}</aside>;
   }
 }
 
