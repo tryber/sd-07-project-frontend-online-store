be86b764a5d161cc01a176a976493585bc8efe8c
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-11-27 15:11:16 -0300

    adiciona produto ao carrinho usando local storage,pra isso cria o arquivo cart na pasta sevice onde ficam as funções interação com o local storage. Cria um itemcart para redenrizar utilizando o arquivo em componentes e adiciona um botão para voltar a homepage

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import '../App.css';
+import { addItemCart, readCartItems} from '../services/Cart'
 
 export default class Item extends React.Component {
+  constructor(){
+    super();
+    this.handleLocalStorage = this.handleLocalStorage.bind(this);
+  }
+  
+    
+  handleLocalStorage (id, title, thumbnail, price) {
+    const item = {id:id, title:title, thumbnail:thumbnail, price:price};
+    addItemCart(item)
+    console.log(readCartItems(), "cart");
+  }
   render() {
+      const  {id, title, thumbnail, price} = this.props
     return (
-      <div className="items" data-testid="product" key={this.props.id}>
-        <h4>{this.props.title}</h4>
-        <img className="item-image" src={this.props.thumbnail}/>
-        <div className="item-price">R$ {this.props.price}</div>
+      <div className="items" data-testid="product" key={id}>
+        <h4>{title}</h4>
+        <img className="item-image" src={thumbnail}/>
+        <div className="item-price">R$ {price}</div>
+        <button className="button"
+        data-testid="product-detail-add-to-cart"
+        onClick={()=>{this.handleLocalStorage(id, title, thumbnail, price)}}>
+          Add Carrinho
+        </button>
       </div>
     )
   }
