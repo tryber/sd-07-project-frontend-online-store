38de9420a87543aea1ff1e394e940283366e607a
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-11-27 13:59:39 -0300

    Resolvendo o Requisito 10

--- src/components/ShoppingCartList/index.js
+++ src/components/ShoppingCartList/index.js
@@ -1,31 +1,53 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import QuantityControl from '../QuantityControl';
+import * as lsapi from '../../services/lsapi';
+import './ShoppingCartList.css';
 
 class ShoppingCartList extends Component {
+  constructor(props) {
+    super(props);
+    this.updateCartList = this.updateCartList.bind(this);
+  }
+
+  updateCartList({ product, quantity }) {
+    if (product) {
+      lsapi.updateShoppingCartList(product, quantity);
+      const { handleChange } = this.props;
+      handleChange();
+    }
+  }
+
   render() {
     const { purchasedProducts } = this.props;
+    const formatter = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
     return (
       <div className="cart-list-content">
         {purchasedProducts.length
           ? purchasedProducts.map(({ product, quantity }) => (
             <div className="cart-list-item" key={ product.id }>
-              <p>
-                Código:
-                { product.id }
-              </p>
+              <button
+                type="submit"
+                onClick={ (e) => {
+                  e.preventDefault(); this.updateCartList({ product, quantity: 0 });
+                } }
+              >
+                X
+              </button>
               <img src={ product.thumbnail } alt="Imagem do produto" />
-              <p data-testid="shopping-cart-product-name">
-                Produto:
+              <span data-testid="shopping-cart-product-name">
                 { product.title }
-              </p>
-              <p data-testid="shopping-cart-product-quantity">
-                Quantidade:
-                { quantity }
-              </p>
-              <p>
-                Valor:
-                { product.price }
-              </p>
+              </span>
+              <QuantityControl
+                item={ { product, quantity } }
+                handleClick={ this.updateCartList }
+              />
+              <span>
+                { formatter.format(product.price * quantity) }
+              </span>
             </div>
           )) : (
             <div data-testid="shopping-cart-empty-message">
@@ -47,6 +69,7 @@ ShoppingCartList.propTypes = {
     }).isRequired,
     quantity: PropTypes.number.isRequired,
   })).isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default ShoppingCartList;
--- src/pages/ShoppingCart/index.js
+++ src/pages/ShoppingCart/index.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import { ShoppingCartList } from '../../components';
 import * as lsapi from '../../services/lsapi';
+import './ShoppingCart.css';
 
 class ShoppingCart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalPrice: 0,
+    };
+    this.updateTotalPrice = this.updateTotalPrice.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateTotalPrice();
+  }
+
+  updateTotalPrice() {
+    this.setState({ totalPrice: lsapi.getTotalPrice() });
+  }
+
   render() {
     const purchasedProducts = lsapi.getShoppingCartList();
+    const { totalPrice } = this.state;
+    const formatter = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
     return (
       <div className="cart-list-container">
+        <h2>Carrinho de Compras</h2>
         <ShoppingCartList
           purchasedProducts={ purchasedProducts }
+          handleChange={ this.updateTotalPrice }
         />
+        <h3>
+          { `Valor Total da Compra: ${formatter.format(totalPrice)}` }
+        </h3>
       </div>
     );
   }
