c713716b47dca66cfa1050294743d84bf01cf0d3
Elano Evaristo <elano3004@gmail.com>
2020-11-27 17:02:12 -0300

    create list-product

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import icon from '../shopping-cart.svg';
+import iconSearch from '../search.svg';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 
 class Header extends React.Component {
+  componentDidUpdate() {
+    const { listProduct } = this.state;
+    this.getProducts(listProduct);
+  }
+
+  async getProducts(key) {
+    const input = document.getElementById('search-input');
+    const response = await getProductsFromCategoryAndQuery(input);
+    this.setState({ [key]: response });
+  }
+
   render() {
     return (
       <div className="container-header">
@@ -11,7 +25,10 @@ class Header extends React.Component {
             <h1>Shopping</h1>
           </div>
           <div className="input-serch">
-            <input type="text" placeholder="Buscar" className="input" />
+            <input type="text" placeholder="Buscar" className="input" id="search-input" data-testid="query-input" />
+            <button className="btn-search" type="submit" onClick={ this.getProducts } data-testid="query-button" >
+              <img src={ iconSearch } alt="Search-icon" />
+            </button>
           </div>
           <div className="cart-icon">
             <Link to="/cart" data-testid="shopping-cart-button">
@@ -23,5 +40,15 @@ class Header extends React.Component {
     );
   }
 }
+
+// Header.defaultProps = {
+//   searchInput: '',
+// };
+
+// Header.propTypes = {
+//   searchInput: PropTypes.string,
+//   onInputSearchChange: PropTypes.func.isRequired,
+//   buttonSearch: PropTypes.func.isRequired,
+// };
 
 export default Header;
