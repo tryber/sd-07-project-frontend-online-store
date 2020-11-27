9f9ac5af2974fb2862717f3870e71294c2ccb6ad
Breno Alves <brenoalv3ss@gmail.com>
2020-11-27 16:52:32 -0300

    Versao atualizada

---
+++ src/Components/ListCardsProduts.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Cart from '../Pages/Cart';
+import { Link } from 'react-router-dom';
+
+
+class ListCardsProduts extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      status : '',
+      shouldRedirect: false,
+      products: []
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+  }
+
+  render() {
+    const { product } = this.props;
+    /* const { status, products, shouldRedirect } = this.state; */
+    return (
+      <div data-testid="product">
+        <p>{product.title}</p>
+        <img src={ product.thumbnail } alt={ `${product.title} sprite` } />
+        <p>{product.price}</p>
+        <button 
+           onClick={this.handleClick}
+        >
+          ADICIONAR NO CARRINHO
+        </button>
+        </div>
+      );
+    }
+  }
+
+export default ListCardsProduts;
