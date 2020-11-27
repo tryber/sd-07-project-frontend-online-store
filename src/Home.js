54eda43407e6dd09dd3d4c9be302db21da3992bc
Camila Valentim <camilaifpb@hotmail.com>
2020-11-27 15:37:22 -0300

    code climate

--- src/Home.js
+++ src/Home.js
@@ -19,37 +19,41 @@ class ProductsList extends Component {
     this.change = this.change.bind(this);
   }
 
-async fecthProducts() {
-  const { demand } = this.state;
-  const resultRequest = await getProductsFromCategoryAndQuery('', demand);
-  this.setState({
+  async fecthProducts() {
+    const { demand } = this.state;
+    const resultRequest = await getProductsFromCategoryAndQuery('', demand);
+    this.setState({
       status: true,
       products: resultRequest.results,
-  });
-}
+    });
+  }
 
-change({ target }) {
-  const { name, value } = target;
-  this.setState({ [name]: value });
-}
+  change({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
     const { status, products } = this.state;
     return (
       <div>
         {(status) ? <ProductCard products={ products } /> : false}
         {(products.length === '') ?  <span>Nenhum produto foi encontrado</span> : false}
-      <div className="ContainerForm">
-        <div>
-          <label htmlFor="inputHome">
-          <div>
-            <input
-              data-testid="query-input"
-              name="demand"
-              className="inputHome"
-              type="text"
-              onChange={this.change}
-            />
-            <button data-testid="query-button" type="button" onClick={ this.fecthProducts }>PESQUISAR</button>
+          <div className="ContainerForm">
+            <div>
+              <label htmlFor="inputHome">
+                <div>
+                  <input
+                  data-testid="query-input"
+                  name="demand"
+                  className="inputHome"
+                  type="text"
+                  onChange={ this.change }
+                 />
+            <button 
+            data-testid="query-button"
+            type="button"
+            onClick={ this.fecthProducts }>PESQUISAR</button>
             <span data-testid="home-initial-message">
               Digite algum termo de pesquisa ou escolha uma categoria.
             </span>
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 
 class ProductCard extends Component {
-    render() {
-        const { products } = this.props
-        return (
-            <div>
-                {products.map((product) => {
-                   const { id, title, thumbnail, price } = product
-                   return (
-                    <div key={ id } data-testid="product">
-                        <h2>{ title }</h2>
-                        <img src={ thumbnail } alt="products" />
-                        <p>{ price }</p>
-                    </div>
-                    
-                   );
-                })}
+  render() {
+    const { products } = this.props;
+    return (
+      <div>
+        {products.map((product) => {
+          const { id, title, thumbnail, price } = product;
+          return (
+            <div 
+            key={ id } 
+            data-testid="product"
+            >
+              <h2>{ title }</h2>
+              <img src={ thumbnail } alt="products" />
+              <p>{ price }</p>
             </div>
-        );
-    }
+                
+    );
+        })}
+      </div>
+    );
+  }
 }
 
 export default ProductCard;
