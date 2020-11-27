d770944c5dfdaf714c2f53077508b30230fa718a
Lucas Nonato de Sá <lucas.nonato1@hotmail.com>
2020-11-27 17:00:13 -0300

    push

--- src/components/BuyerInfo.jsx
+++ src/components/BuyerInfo.jsx
@@ -7,53 +7,71 @@ class BuyerInfo extends React.Component {
       name: '',
       cpf: '',
       email: '',
-      telefone: '',
+      phone: '',
       cep: '',
       address: '',
       other: '',
       number: 0,
       city: '',
       stateCountry: '',
-    }
+    };
   }
+
+  changeHandler({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
+    const {
+      name,
+      cpf,
+      email,
+      phone,
+      cep,
+      address,
+      other,
+      number,
+      city,
+      stateCountry,
+    } = this.state;
     return (
       <div className="form">
         <h2>Informações do Comprador</h2>
         <div className="form-group">
           <div>
-            <input type="text" placeholder="Nome" />
+            <input name="name" type="text" placeholder="Nome" value={name} />
           </div>
           <div>
-            <input type="text" placeholder="CPF" />
+            <input name="cpf" type="text" placeholder="CPF" value={cpf} />
           </div>
           <div>
-            <input type="email" placeholder="Email" />
+            <input name="email" type="email" placeholder="Email" value={email} />
           </div>
           <div>
-            <input type="text" placeholder="Telefone" />
-          </div>
-        </div>
-        <div className="form-group">
-          <div>
-            <input type="text" placeholder="CEP" />
-          </div>
-          <div>
-            <input type="text" placeholder="Endereço" />
+            <input name="telefone" type="text" placeholder="Telefone" value={phone} />
           </div>
         </div>
         <div className="form-group">
           <div>
-            <input type="text" placeholder="Complemento" />
+            <input name="cep" type="text" placeholder="CEP" value={cep} />
           </div>
           <div>
-            <input type="text" placeholder="Número" />
+            <input name="address" type="text" placeholder="Endereço" value={address} />
+          </div>
+        </div>
+        <div className="form-group">
+          <div>
+            <input name="other" type="text" placeholder="Complemento" value={other} />
           </div>
           <div>
-            <input type="email" placeholder="Cidade" />
+            <input name="number" type="text" placeholder="Número" value={number} />
           </div>
           <div>
-            <input type="select" placeholder="Estado" />
+            <input type="email" placeholder="Cidade" value={city} />
+          </div>
+          <div>
+            <input type="select" placeholder="Estado" value={stateCountry} />
           </div>
         </div>
       </div>
