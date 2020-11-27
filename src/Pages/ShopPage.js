e6ecd49607ed86925f9f2ee206ff2e3aabf1bacb
Bruna Sepulveda <sepulvedabruna@gmail.com>
2020-11-27 16:06:00 -0300

    issues

---
+++ src/Pages/ShopPage.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import EmptyShopCar from '../components/EmptyShopCar';
+import ItemCart from '../components/ItemCart';
+import { readCartItems } from '../services/Cart';
+
+class ShopPage extends React.Component {
+  constructor() {
+    super();
+    this.getLocalStorage = this.getLocalStorage.bind(this);
+    this.state = {
+      itemsCart: [],
+    };
+  }
+
+  componentDidMount() {
+    getLocalStorage();
+  }
+
+  getLocalStorage() {
+    const items = readCartItems();
+    this.setState({ itemsCart: items });
+  }
+
+  render() {
+    const { itemsCart } = this.state;
+    let conteudo;
+    if (itemsCart.length > 0) {
+      conteudo = <div className="items-list">
+        {itemsCart.map((product) =>
+          <ItemCart
+            key={ product.id }
+            title={ product.title }
+            thumbnail={ product.thumbnail }
+            price={ product.price } />)}
+      </div>
+    } else {
+      conteudo = <EmptyShopCar />;
+    }
+    return (
+      <div>
+        {conteudo}
+        <Link className="button" to="/"> Home</Link>
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
