6789f5d58583674ea7fbf315b9c86621c4b5990d
Camila Valentim <camilaifpb@hotmail.com>
2020-11-28 16:03:19 -0300

    requisito v1

--- src/Home.js
+++ src/Home.js
@@ -40,14 +40,8 @@ class ProductsList extends Component {
         {(status) ? <ProductCard products={ products } /> : false}
         {(products.length === '') ? <span>Nenhum produto foi encontrado</span> : false}
         <div className="ContainerForm">
-
-  render() {
-    const value = '';
-    return (
-      <div className="ContainerForm">
-        <form>
-          <div>
-            <label htmlFor="inputHome">
+          <form>
+            <div>
               <div>
                 <input
                   data-testid="query-input"
@@ -63,29 +57,11 @@ class ProductsList extends Component {
                 >
                   PESQUISAR
                 </button>
-                  value={ value }
-                  className="inputHome"
-                  type="text"
-                  // onChange={ onChange }
-                />
                 <span data-testid="home-initial-message">
                   Digite algum termo de pesquisa ou escolha uma categoria.
                 </span>
               </div>
-            </label>
-          </div>
-          <div>
-            <Link to="./pages/cart">
-              <img
-                className="btImg"
-                data-testid="shopping-cart-button"
-                src={ cart }
-                alt="Carrinho de Compras"
-              />
-            </Link>
-          </div>
-        </div>
-        <Categories />
+            </div>
             <div>
               <Link to="./pages/cart">
                 <img
@@ -96,18 +72,24 @@ class ProductsList extends Component {
                 />
               </Link>
             </div>
-          </div>
-          <Categories />
-        </form>
+            <Categories>
+              <div>
+                <Link to="./pages/cart">
+                  <img
+                    className="btImg"
+                    data-testid="shopping-cart-button"
+                    src={ cart }
+                    alt="Carrinho de Compras"
+                  />
+                </Link>
+              </div>
+            </Categories>
+          </form>
+        </div>
       </div>
     );
   }
 }
-
-
- Button.propTypes = {
-  onChange: PropTypes.func.isRequired,
- };
 
 
 export default ProductsList;
