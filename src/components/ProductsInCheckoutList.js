ce0826f99add4c9a4dca99174910f7fcc2d736af
dennerpl <denner.souzapl@gmail.com>
2020-11-28 18:28:29 -0300

    Tela e formulário de checkout feitos

---
+++ src/components/ProductsInCheckoutList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import IndividualCard from './IndividualCard';
+import * as cartFunctions from '../services/cartFunctions';
+
+class ProductsInCheckoutList extends Component {
+  constructor() {
+    super();
+    const initialNumber = 0;
+    this.state = {
+      shoppingCartItems: cartFunctions.recoveryProductsFromLocalStorage(),
+      totalPrice: cartFunctions
+        .recoveryProductsFromLocalStorage()
+        .reduce((totalPrice, product) => totalPrice + product.price, initialNumber),
+    };
+  }
+
+  render() {
+    const { shoppingCartItems, totalPrice } = this.state;
+    const numberOfHouses = 2;
+    return (
+      <section>
+        <h3>Revise seus produtos</h3>
+        <div>
+          {shoppingCartItems.map((product) => (
+            <IndividualCard
+              key={ product.id }
+              id={ product.id }
+              title={ product.title }
+              price={ product.price }
+              image={ product.thumbnail }
+            />
+          ))}
+        </div>
+        <p>{`Total: R$ ${totalPrice.toFixed(numberOfHouses)}`}</p>
+      </section>
+    );
+  }
+}
+
+export default ProductsInCheckoutList;
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import CheckoutForm from '../components/CheckoutForm';
+import ProductsInCheckoutList from '../components/ProductsInCheckoutList';
+import * as cartFunctions from '../services/cartFunctions';
 
 class Checkout extends Component {
+  constructor() {
+    super();
+    this.state = {
+      shoppingCartitems: cartFunctions.recoveryProductsFromLocalStorage(),
+    };
+  }
+
   render() {
+    const { shoppingCartitems } = this.state;
     return (
-      <div>Página de finalização da compra</div>
+      <div>
+        <Link to="/">
+          <button type="button">Página Inicial</button>
+        </Link>
+        <ProductsInCheckoutList shoppingCartitems={shoppingCartitems} />
+        <CheckoutForm />
+      </div>
     );
   }
 }
