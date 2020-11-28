18e0943b0d62ea9d553fb3161267b2f4ae23641d
Rogerio-feitor <roger.feitor@gmail.com>
2020-11-27 21:31:34 -0300

    Requisito 7 resolvido

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
-import ProductDetails from './pages/ProductDetails'
+import ProductDetails from './pages/ProductDetails';
+
 
 function App() {
   return (
@@ -11,8 +12,8 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/pages/ShoppingCart" component={ ShoppingCart } />
-          <Route exact path="/" component={ Home } />
           <Route path="/pages/ProductDetails/:id" component={ ProductDetails } />
+          <Route exact path="/" component={ Home } />
         </Switch>
       </BrowserRouter>
     </div>
