b2941513ee65ee8e6d9e8d88c9f6eda29466f117
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-28 12:57:22 -0300

    r 5ok

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
   render() {
+    const { handleChange, onClick } = this.props;
     return (
       <div>
         <input
           data-testid="query-input"
           type="search"
-          name="order"
-          onChange={ this.onChange }
+          name="query"
+          onChange={ (event) => handleChange(event) }
         />
+        <button
+          data-testid="query-button"
+          type="button"
+          onClick={ onClick }
+        >
+          BUSCAR
+        </button>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
@@ -17,5 +26,10 @@ class SearchBar extends Component {
     );
   }
 }
+
+SearchBar.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
 
 export default SearchBar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Category from '../components/Category';
+import NotFound from '../components/NotFound';
+import ProductCard from '../components/ProductCard';
 import SearchBar from '../components/SearchBar';
 import ShoppingCartButton from '../components/ShoppingCartButton';
 import * as api from '../services/api';
@@ -7,24 +9,61 @@ import * as api from '../services/api';
 class Home extends Component {
   constructor() {
     super();
+    this.onClick = this.onClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.state = {
       categories: [],
+      products: [],
+      category: '',
+      query: '',
+      status: false,
+      notFound: false,
     };
   }
 
   componentDidMount() { this.fetchCategories(); }
 
+
+  onClick() {
+    this.fetchProducts();
+  }
+
   async fetchCategories() {
     this.setState({ categories: await api.getCategories() });
   }
 
+  async fetchProducts() {
+    const { category, query } = this.state;
+    const resultRequest = await api.getProductsFromCategoryAndQuery(category, query);
+    const empytArray = 0;
+    if (resultRequest.results.length === empytArray) {
+      this.setState({
+        notFound: true,
+      });
+    } else {
+      this.setState({
+        products: resultRequest.results,
+        status: true,
+      });
+    }
+  }
+
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   render() {
-    const { categories } = this.state;
+    const { categories, products, status, notFound } = this.state;
+    console.log(notFound);
     return (
       <div>
-        <SearchBar />
+        <SearchBar handleChange={ this.handleChange } onClick={ this.onClick } />
+        {notFound ? <NotFound /> : false}
         <ShoppingCartButton />
         <Category categories={ categories } />
+        {status ? <ProductCard products={ products } /> : false}
       </div>
     );
   }
