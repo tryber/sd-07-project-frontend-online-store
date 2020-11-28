f56b51d393893c9806699b84141fb6864fbad9b6
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-27 22:59:24 -0300

    r4 ok

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
+import Category from '../components/Category';
 import SearchBar from '../components/SearchBar';
 import ShoppingCartButton from '../components/ShoppingCartButton';
+import * as api from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+  }
+
+  componentDidMount() { this.fetchCategories(); }
+
+  async fetchCategories() {
+    this.setState({ categories: await api.getCategories() });
+  }
+
   render() {
+    const { categories } = this.state;
     return (
       <div>
         <SearchBar />
         <ShoppingCartButton />
+        <Category categories={ categories } />
       </div>
     );
   }
