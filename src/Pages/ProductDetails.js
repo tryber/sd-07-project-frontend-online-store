58c8d49f6d4047fcde40dbea52ce1dfd18fecb0f
Strongreen <strongreen@live.com>
2020-11-28 00:29:46 -0300

    Estilizando o restante das telas, adicionando algumas melhorias

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import '../App.css';
 import * as api from '../services/api';
 class ProductDetails extends Component {
@@ -21,40 +22,52 @@ class ProductDetails extends Component {
   async getDetailsProduct() {
     const parametros = this.props.match.params.parametros;
 
-    const categoria = parametros.split("-")[0];
-    const termo = parametros.split("-")[1];
-    const id = parametros.split("-")[2];
+    const categoria = parametros.split('-')[0];
+    const termo = parametros.split('-')[1];
+    const id = parametros.split('-')[2];
 
     const { results } = await api.getProductsFromCategoryAndQuery(
       categoria,
       termo
     );
-    const produto = results.filter(
-      (productOne) => productOne.id === id
-    );
+    const produto = results.filter((productOne) => productOne.id === id);
     const marca = produto[0].attributes[0].value_name;
     const condicoes = produto[0].attributes[1].value_name;
     const modelo = produto[0].attributes[2].value_name;
     this.setState({ product: produto[0], marca, modelo, condicoes });
- }
+  }
 
   render() {
-    const { product, marca = "Nao informado", modelo = "Nao informado", condicoes = "Nao informado" } = this.state;
+    const {
+      product,
+      marca = 'Nao informado',
+      modelo = 'Nao informado',
+      condicoes = 'Nao informado',
+    } = this.state;
     const { title, thumbnail, price } = product;
     return (
-      <div className="product-card-details">
-        <h1 data-testid="product-detail-name">{title}</h1>
-        <div className="product-card-image-dev">
-          <img
-            className="product-card-image-details"
-            alt="Imagem do Produto"
-            src={thumbnail}
-          />
+      <div>
+        <Link to="/">
+        <img
+          alt="Voltar"
+          src="https://img.icons8.com/ios/2x/reply-arrow.png"
+        />
+        </Link>
+        <div className="product-card-details">
+          <h1 data-testid="product-detail-name">{title}</h1>
+          <div className="product-card-image-dev">
+            <img
+              className="product-card-image-details"
+              alt="Imagem do Produto"
+              src={thumbnail}
+            />
+          </div>
+          <p>R$ {price}</p>
+          <p>Marca: {marca}</p>
+          <p>Modelo: {modelo} </p>
+          <p>Condições do Produto: {condicoes}</p>
+          <button data-testid="product-detail-add-to-cart" className="button-product">ADICIONAR NO CARRINHO</button>
         </div>
-        <p>R$ {price}</p>
-        <p>Marca: {marca}</p>
-        <p>Modelo: {modelo} </p>
-        <p>Condições do Produto: {condicoes}</p>
       </div>
     );
   }
