dbebee2c6f1191ec0d0b6c5ea3dd19a328a1f3ff
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-11-27 22:44:54 -0300

    r3 ok

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css';
-// import Details from './pages/Details';
-// import ShoppingCart from './pages/ShoppingCard';
+import ShoppingCart from './pages/ShoppingCard';
+
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
+        <Route exact path="/shoppingCart" component={ ShoppingCart } />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../components/SearchBar';
+import ShoppingCartButton from '../components/ShoppingCartButton';
 
 class Home extends Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <ShoppingCartButton />
       </div>
     );
   }
