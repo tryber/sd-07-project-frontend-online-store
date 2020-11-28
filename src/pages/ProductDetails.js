8bd96a94ddae751445d9e6e16de5d388527bf512
moises <moisaant@gmail.com>
2020-11-28 00:47:44 -0300

    esqueci de commitar/funciona mas nao passa

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -2,9 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class ProductDetails extends React.Component {
+  constructor() {
+    super();
+    this.addInCart = this.addInCart.bind(this);
+  }
+
+  addInCart(title) {
+    if (localStorage.getItem(title)) {
+      const amount = JSON.parse(localStorage.getItem(title)) + 1;
+      localStorage.setItem(title, amount);
+    } else {
+      localStorage.setItem(title, 1);
+    }
+  }
+
   render() {
     const { location } = this.props;
     const { title, thumbnail, price, attributes } = location.product;
+
     return (
       <div>
         <div>
@@ -13,6 +28,15 @@ class ProductDetails extends React.Component {
             <span>{ price }</span>
           </h1>
           <img src={ thumbnail } alt="Ilustração produto" />
+        </div>
+        <div>
+          <button
+            data-testid="product-detail-add-to-cart"
+            type="button"
+            onClick={() => this.addInCart(title)}
+          >
+            Adicionar ao Carrinho
+          </button>
         </div>
         <ul>
           <h2>Especificações Técnicas</h2>
