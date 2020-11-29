0d6d692a7f790cbf990883bb478ba1fde25efc88
Alana Monteiro <lanaah.15@gmail.com>
2020-11-29 18:31:54 -0300

    product details css

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../App.css';
 
 class ProductDetails extends React.Component {
   render() {
@@ -7,8 +8,15 @@ class ProductDetails extends React.Component {
     const { product } = location;
     const { item } = product;
     return (
-      <div>
-        <h1 data-testid="product-detail-name">{item.title}</h1>
+      <div className="page-container">
+        <div className="page-sub-container">
+          <div className="product-container">
+            <img alt="Imagem do produto" src={ item.thumbnail } />
+            <h2 data-testid="product-detail-name">{ item.title }</h2>
+            <p>{ `R$ ${item.price}` }</p>
+          </div>
+        </div>
+          {/* <button >Adicionar</button> */}
       </div>
     );
   }
@@ -19,6 +27,8 @@ ProductDetails.propTypes = {
     product: PropTypes.shape({
       item: PropTypes.shape({
         title: PropTypes.string.isRequired,
+        thumbnail: PropTypes.string.isRequired,
+        price: PropTypes.string.isRequired,
       }).isRequired,
     }).isRequired,
   }).isRequired,
