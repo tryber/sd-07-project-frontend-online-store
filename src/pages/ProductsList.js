7dc0b3b7aa1ad3b6d8e27435c96877ffea1c884e
Elisa <elisalibanio1@gmail.comgit config --global user.email elisalibanio1@gmail.com>
2020-11-29 20:14:31 -0300

    Revert "requisitos 10 e 11"
    This reverts commit a9a6bae40086eb8679ccf70b95827a91fcaf2eb1.

--- src/pages/ProductsList.js
+++ src/pages/ProductsList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import CartButton from './CartButton';
-import { handleAddItemToCart } from '../services/utils';
 
 export default class ProductsList extends React.Component {
   render() {
@@ -10,32 +9,30 @@ export default class ProductsList extends React.Component {
     return (
       <ul>
         {productList.length
-          ? productList.map((product) => (
+          ? productList.map(({ id, title, thumbnail, price }) => (
             <li
-              key={ product.id }
+              key={ id }
               data-testid="product"
             >
               <Link
                 to={ {
                   pathname: '/Product',
                   state: {
-                    product,
+                    productName: title,
+                    productImg: thumbnail,
+                    productPrice: price,
                   },
                 } }
                 data-testid="product-detail-link"
-                key={ `${product.title} ${product.id}` }
+                key={ `${title} ${id}` }
               >
                 <div>
-                  <h4>{ product.title }</h4>
-                  <img src={ product.thumbnail } alt="Product" />
-                  <p>{ product.price }</p>
+                  <h4>{ title }</h4>
+                  <img src={ thumbnail } alt="Product" />
+                  <p>{ price }</p>
                 </div>
               </Link>
-              <CartButton
-                datatestid="product-add-to-cart"
-                product={ product }
-                addItemToCart={ handleAddItemToCart }
-              />
+              <CartButton datatestid="product-add-to-cart" productName={ title } />
             </li>
           )) : (<li> Nenhum produto foi encontrado </li>)}
       </ul>
