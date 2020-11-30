69142b7ed6cc0da8b03589f0169232b266575463
Camila Valentim <camilaifpb@hotmail.com>
2020-11-29 23:02:17 -0300

    requisito 6

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import * as api from '../services/api';
+
 
 class Categories extends React.Component {
   constructor() {
@@ -7,24 +9,51 @@ class Categories extends React.Component {
     this.state = {
       myCategories: [],
     };
+    this.getProductCategory = this.getProductCategory.bind(this);
   }
+
 
   componentDidMount() {
     api.getCategories().then((categories) => this.setState({ myCategories: categories }));
   }
 
+  getProductCategory(id) {
+    const { fecthProductsCategoryId } = this.props;
+    fecthProductsCategoryId(id);
+  }
+
   makeList(list) {
-    return <li data-testid="category" key={ list.id }>{list.name}</li>;
+    return list.map(({ id, name }) => (
+      <p key={ id }>
+        <label htmlFor={ id }>
+          <input
+            id={ id }
+            onClick={ () => this.getProductCategory(id) }
+            name="productId"
+            type="radio"
+            data-testid="category"
+            value={ id }
+          />
+          { name }
+        </label>
+      </p>
+    ));
   }
 
   render() {
     const { myCategories } = this.state;
     return (
       <div>
-        {myCategories.map((categorie) => this.makeList(categorie))}
+        {this.makeList(myCategories)}
+
       </div>
     );
   }
 }
+
+Categories.propTypes = {
+  fecthProductsCategoryId: PropTypes.func.isRequired,
+};
+
 
 export default Categories;
