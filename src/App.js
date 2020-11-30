0158ac854c377f75638081fed18c1554b738405d
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-11-30 15:14:47 -0300

    Adicionando requisito 8

--- src/App.js
+++ src/App.js
@@ -5,18 +5,38 @@ import { ShoppingCart, ProductDetails } from './pages/index';
 import Home from './pages/Home';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.addCart = this.addCart.bind(this);
+    this.state = { cartItems: [] };
+  }
+
+  addCart(product) {
+    this.setState(({ cartItems }) => ({
+      cartItems: [...cartItems, product],
+    }));
+  }
+
   render() {
+    const { cartItems } = this.state;
     return (
       <div className="App">
         <header className="App-header">TrybeLibre Online Store</header>
         <BrowserRouter>
           <Switch>
-            <Route path="/cart" component={ ShoppingCart } />
+            <Route
+              path="/cart"
+              render={ (props) => <ShoppingCart { ...props } cartItems={ cartItems } /> }
+            />
             <Route
               path="/productdetails/:id"
               render={ (props) => <ProductDetails id={ props.match.params.id } /> }
             />
-            <Route exact path="/" component={ Home } />
+            <Route
+              exact
+              path="/"
+              render={ (props) => <Home { ...props } addCart={ this.addCart } /> }
+            />
           </Switch>
         </BrowserRouter>
       </div>
