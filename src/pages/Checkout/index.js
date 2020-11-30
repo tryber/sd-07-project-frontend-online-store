1378710463593272a34b44c4e99bd8147a814551
gustasaideles <gustasaideles.gmail.com>
2020-11-30 17:21:05 -0300

    arrumando erros de lint

--- src/pages/Checkout/index.js
+++ src/pages/Checkout/index.js
@@ -5,20 +5,20 @@ class Checkout extends Component {
   constructor() {
     super();
 
-  this.validateForm = this.validateForm.bind(this);
+    this.validateForm = this.validateForm.bind(this);
   }
 
 
   validateForm() {
-    let x = document.forms["checkOutForm"]["nameInput"].value;
-    if (x == "") {
-      alert("O campo nome precisa ser preenchido");
+    const x = document.forms.checkOutForm.nameInput.value;
+    if (x === '') {
+      alert('O campo nome precisa ser preenchido');
       return false;
     }
   }
 
   render() {
-    return(
+    return (
       <section>
         <header>
           <h3>Revise seus Produtos</h3>
@@ -31,101 +31,139 @@ class Checkout extends Component {
             action="/"
             onSubmit={ this.validateForm }
           >
-            <label for="nameInput">Nome Completo</label>
-            <input
-              type="text"
-              data-testid="checkout-fullname"
-              id="nameInput"
-              name="nameInput"
-            /><br/>
-            <label for="cpfInput">CPF</label>
-            <input
-              type="text"
-              data-testid="checkout-cpf"
-              id="cpfInput"
-              name="cpfInput"
-            /><br/>
-            <label for="emailInput">Email</label>
-            <input
-              type="email"
-              data-testid="checkout-email"
-              id="emailInput"
-              name="emailInput"
-            /><br/>
-            <label for="telInput">Telefone</label>
-            <input
-              type="tel"
-              data-testid="checkout-phone"
-              id="telInput"
-              name="telInput"
-            /><br/>
-            <label for="cepInput">CEP</label>
-            <input
-              type="text"
-              data-testid="checkout-cep"
-              id="cepInput"
-              name="cepInput"
-            /><br/>
-            <label for="addressInput">Endereco</label>
-            <input
-              type="text"
-              data-testid="checkout-address"
-              id="addressInput"
-              name="addressInput"
-            /><br/>
-            <label for="complementInput">Complemento</label>
-            <input
-              type="text"
-              id="complementInput"
-              name="complementInput"
-            /><br/>
-            <label for="numberInput">Numero</label>
-            <input
-              type="number"
-              id="numberInput"
-              name="numberInput"
-            /><br/>
-            <label for="cityInput">Cidade</label>
-            <input
-              type="text"
-              id="cityInput"
-              name="cityInput"
-            /><br/>
-            <label for="stateInput">Estado</label>
-            <input
-              type="text"
-              id="stateInput"
-              name="stateInput"
-            /><br/>
-            <h3>Metodo de Pagamento</h3>
-            <input
-              type="radio"
-              id="bankSlip"
-              name="paymentMethod"
-            />
-            <label for="bankSlip">Boleto</label>
+            <label htmlFor="nameInput">
+              Nome Completo
+              <input
+                type="text"
+                data-testid="checkout-fullname"
+                id="nameInput"
+                name="nameInput"
+              />
+            </label>
+            <br />
+            <label htmlFor="cpfInput">
+              CPF
+              <input
+                type="text"
+                data-testid="checkout-cpf"
+                id="cpfInput"
+                name="cpfInput"
+              />
+            </label>
+            <br />
+            <label htmlFor="emailInput">
+              Email
+              <input
+                type="email"
+                data-testid="checkout-email"
+                id="emailInput"
+                name="emailInput"
+              />
+            </label>
+            <br />
+            <label htmlFor="telInput">
+              Telefone
+              <input
+                type="tel"
+                data-testid="checkout-phone"
+                id="telInput"
+                name="telInput"
+              />
+            </label>
+            <br />
+            <label htmlFor="cepInput">
+              CEP
+              <input
+                type="text"
+                data-testid="checkout-cep"
+                id="cepInput"
+                name="cepInput"
+              />
+            </label>
+            <br />
+            <label htmlFor="addressInput">
+              Endereco
+              <input
+                type="text"
+                data-testid="checkout-address"
+                id="addressInput"
+                name="addressInput"
+              />
+            </label>
+            <br />
+            <label htmlFor="complementInput">
+              Complemento
+              <input
+                type="text"
+                id="complementInput"
+                name="complementInput"
+              />
+            </label>
+            <br />
+            <label htmlFor="numberInput">
+              Numero
+              <input
+                type="number"
+                id="numberInput"
+                name="numberInput"
+              />
+            </label>
+            <br />
+            <label htmlFor="cityInput">
+              Cidade
+              <input
+                type="text"
+                id="cityInput"
+                name="cityInput"
+              />
+            </label>
+            <br />
+            <label htmlFor="stateInput">
+              Estado
+              <input
+                type="text"
+                id="stateInput"
+                name="stateInput"
+              />
+            </label>
+            <br />
             <div>
-              <h4>Cartao de Credito</h4>
+              <h3>Metodo de Pagamento</h3>
               <input
                 type="radio"
-                id="visa"
+                id="bankSlip"
                 name="paymentMethod"
               />
-              <label for="visa">Visa</label>
-              <input
-                type="radio"
-                id="master"
-                name="paymentMethod"
-              />
-              <label for="master">MasterCard</label>
-              <input
-                type="radio"
-                id="elo"
-                name="paymentMethod"
-              />
-              <label for="elo">Elo</label>
+              <label htmlFor="bankSlip">
+                Boleto
+                <h4>Cartao de Credito</h4>
+                <input
+                  type="radio"
+                  id="visa"
+                  name="paymentMethod"
+                />
+              </label>
+              <label htmlFor="visa">
+                Visa
+                <input
+                  type="radio"
+                  id="master"
+                  name="paymentMethod"
+                />
+              </label>
+              <label htmlFor="master">
+                MasterCard
+                <input
+                  type="radio"
+                  id="elo"
+                  name="paymentMethod"
+                />
+              </label>
+              <label htmlFor="elo">
+                Elo
+                <input type="submit" value="Comprar" />
+              </label>
             </div>
-            <input type="submit" value="Comprar" />
           </form>
         </section>
       </section>
