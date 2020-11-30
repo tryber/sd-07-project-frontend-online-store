6ae460eb68933163fcf4614e73e0179b612a2776
TIAGO <tiagoesdrasvieira@gmail.com>
2020-11-30 18:10:52 -0300

    Test 1

--- src/Pages/Checkout.js
+++ src/Pages/Checkout.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import CheckoutForm from '../Components/CheckoutForm';
-import ListProductsCheckout from '../Components/ListProductsCheckout';
-import Header from '../Components/Header';
+import Teste from '../Components/Teste';
+// import ListProductsCheckout from '../Components/ListProductsCheckout';
+// import Header from '../Components/Header';
 
 class Checkout extends React.Component {
   render() {
     return (
       <div>
-         <Header />
+        {// <ListProductsCheckout />
+        }
+        <Teste />
       </div>
     );
   }
