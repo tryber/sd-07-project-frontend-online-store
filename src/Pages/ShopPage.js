34fa2c1c2903fd5d15fabb13f037f9da3aee46d2
Vanessa Bernardi Bidinotto <35221680+vanessaberbidi@users.noreply.github.com>
2020-11-30 13:04:03 -0300

    requisito 8

--- src/Pages/ShopPage.js
+++ src/Pages/ShopPage.js
@@ -3,13 +3,30 @@ import EmptyShopCar from '../components/EmptyShopCar';
 import { readItemsOnCart } from '../services/cartAPI';
 
 class ShopPage extends React.Component {
+  constructor() {
+    super();
+    this.renderProducts = this.renderProducts.bind(this);
+  }
+
+  renderProducts() {
+    const items = readItemsOnCart();
+    return (
+      <div>
+        {items
+          .map(({ title, id }) => (
+            <div data-testid="shopping-cart-product-name" key={ id }>{ title }</div>
+          ))}
+      </div>
+    );
+  }
+
   render() {
     const items = readItemsOnCart();
     return (
       <>
         <EmptyShopCar />
         <div data-testid="shopping-cart-product-quantity">{items.length}</div>
-        {items.map(({ title, id }) => <div data-testid="shopping-cart-product-name" key={ id }>{ title }</div>)}
+        {this.renderProducts()}
       </>
     );
   }
