3d56371dc21ca3f0f9cd53a038a79b6f3d5a8317
moraisgabri <gabriel8azevedo@gmail.com>
2020-11-30 17:24:24 -0300

    comitaço de requisito 11 funcionando.

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -1,32 +1,35 @@
-import React from 'react';
-import { useLocation } from 'react-router';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import ShoopingCartButton from '../Components/ShoppingCartButton';
+import AvaliationForms from '../Components/AvaliationForms';
 
 const ProductDetails = () => {
   const location = useLocation();
   const { product } = location.state;
   const { title, price, thumbnail, description } = product;
-
-  const addItemToCart = (newTitle, newPrice) => {
-    const newProduct = {
-      title: newTitle,
-      price: newPrice,
-      quantity: 1,
-    };
-    if (localStorage.getItem('products') || []) {
-      let localStorageArray = JSON.parse(localStorage.getItem('products'));
-      if (localStorageArray === null) {
-        localStorageArray = [];
-      }
-      if (localStorageArray !== []) {
-        const existingProduct = localStorageArray
-          .find((productArray) => productArray.title === title);
-        if (existingProduct !== undefined) {
-          existingProduct.quantity += 1;
-        } else { localStorageArray.push(newProduct); }
-      }
-      return localStorage.setItem('products', JSON.stringify(localStorageArray));
+  const addProductToCart = (productt) => {
+    if (localStorage.getItem(productt)) {
+      const quantity = JSON.parse(localStorage.getItem(productt)) + 1;
+      localStorage.setItem(productt, quantity);
+    } else {
+      localStorage.setItem(productt, 1);
     }
+  };
+
+  const [currentName, setCurrentName] = useState('');
+  const onChangeName = (event) => {
+    setCurrentName(event.target.value);
+  };
+
+  const [currentComment, setCurrentComment] = useState('');
+  const onChangeComment = (event) => {
+    setCurrentComment(event.target.value);
+  };
+
+  const [comments, setComment] = useState([]);
+  const submitComment = (event) => {
+    event.preventDefault();
+    setComment([...comments, { name: currentName, comment: currentComment }]);
   };
 
   return (
@@ -39,10 +42,33 @@ const ProductDetails = () => {
       <button
         type="button"
         data-testid="product-detail-add-to-cart"
-        onClick={ () => addItemToCart(title, price) }
+        onClick={ () => addProductToCart(title) }
       >
         Adicionar ao carrinho
       </button>
+
+      <AvaliationForms
+        onChangeName={ onChangeName }
+        onChangeComment={ onChangeComment }
+        submitComment={ submitComment }
+      />
+
+      <ul className="comment-session">
+        { comments.map((element) => {
+          const { name, comment } = element;
+          return (
+            <li key={ `${name}-comment` }>
+              <span>
+                Nome:
+                { name }
+              </span>
+              <p>
+                { comment }
+              </p>
+            </li>
+          );
+        }) }
+      </ul>
     </div>);
 };
 
