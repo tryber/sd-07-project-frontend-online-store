06c0145ae2a81587eeabc6562eee155180b9009e
marcelo ivan <sagacello@hotmail.com>
2020-11-30 00:49:02 -0300

    requisto 9 feito mas não passa no teste

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import EditForm from '../components/EditForm'
+import EditForm from '../components/EditForm';
 
 export default class ProductDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      objeto: [],
+    };
+    this.addToCart = this.addToCart.bind(this);
+  }
+
+  addToCart() {
+    const {
+      location: { state: product },
+    } = this.props;
+    // aqui as props estão sendo passadas pelo location 
+    // location são props advindas do react dom ?
+    const { id, title, price, thumbnail } = product;
+    const array = JSON.parse(localStorage.getItem('items') || '[]');
+    const index = array.findIndex((elemento) => elemento.id === id);
+    index === -1
+      ? array.push({ id, title, price, thumbnail, quantidade: 1 })
+      : array[index].quantidade++;
+    localStorage.setItem('items', JSON.stringify(array));
+  }
+
   render() {
     const {
       location: { state: product },
+      // o proprio product entra como props e é passado como props
     } = this.props;
     return (
       <div>
@@ -16,7 +40,15 @@ export default class ProductDetail extends React.Component {
         <div>
           <p>{`Preço: $ ${product.price}`}</p>
         </div>
-        < EditForm />
+        <EditForm />
+        <button
+          type="button"
+          data-testid="product-detail-add-to-cart"
+          // id={id}
+          onClick={() => this.addToCart(product)}
+        >
+          Adicionar ao carrinho
+        </button>
       </div>
     );
   }
