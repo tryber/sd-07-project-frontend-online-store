150a81bdefea51d97e6ac642e82765db226571fb
brendalima <brenda.c.lima@poli.ufrj.br>
2020-11-30 18:52:29 -0300

    fixing some issues

--- src/components/AddReview.js
+++ src/components/AddReview.js
@@ -3,26 +3,28 @@ import React, { Component } from 'react';
 class AddReview extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       review: {
-        rating: "",
-        comment: "",
+        rating: '',
+        comment: '',
       },
       history: [],
-     };
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.updateState = this.updateState.bind(this);
   }
 
   updateState(field, newValue) {
-    this.setState({review: {[field]: newValue,},});
+    this.setState({ review: { [field]: newValue, } });
   }
 
   handleSubmit() {
-    this.state.history.push(this.state.review);
+    const { review, history } = this.state,
+    this.state.history.push(review);
   }
 
   render() {
+    const { history } = this.state;
     return (
       <div>
         <h2>Avaliação</h2>
@@ -30,34 +32,32 @@ class AddReview extends Component {
           <textarea 
             placeholder="Escreva um comentário (opcional)"
             data-testid="product-detail-evaluation"
-            onChange={(event) => this.updateState('comment', event.target.value)}
+            onChange={ (event) => this.updateState('comment', event.target.value) }
           />
           <input
             placeholder="Avalie"
             id="evaluation"
             type="number"
             width="100px"
-            min={1}
-            max={5}
+            min={ 1 }
+            max={ 5 }
             required="true"
-            onChange={(event) => this.updateState('rating', event.target.value)}
+            onChange={ (event) => this.updateState('rating', event.target.value) }
           />
-          <label htmlFor="evaluation">Avaliação</label>
-          <button 
-            type="button" 
-            onClick={this.handleSubmit}
-          >Avaliar
+          <button
+            type="button"
+            onClick={ this.handleSubmit }
+          >
+            Avaliar
           </button>
         </form>
         <div>
-          {this.state.history.map((item, index) => {
-            return (
-              <div key={index}>
-                <p>{item.comment}</p>
-                <p>{item.rating}</p>
-              </div>
-            )
-          })}
+          { history.map((item, index) => (
+            <div key={index}>
+              <p>{item.comment}</p>
+              <p>{item.rating}</p>
+            </div>))
+          }
         </div>
       </div>
     )
