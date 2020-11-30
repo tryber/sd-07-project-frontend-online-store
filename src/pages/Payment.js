514868e7af665b5002c2b94e67e1cefac0f6ec57
Felipe Nascimento <flpnascto@gmail.com>
2020-11-30 16:54:57 -0300

    Recuperando items do carrinho (localStorage) na página de finalizar compra.

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,22 +1,42 @@
 import React from 'react';
 
 import UserInfoForm from '../components/UserInfoForm';
+import ListItem from '../components/ListItem';
+import * as cartAPI from '../services/localStorageHandler';
 
 class Payment extends React.Component {
   constructor() {
     super();
 
     this.handleUserInfo = this.handleUserInfo.bind(this);
+    this.totalCartPrice = this.totalCartPrice.bind(this);
   }
+
 
   handleUserInfo(userInfo) {
     console.log(userInfo);
   }
 
+  totalCartPrice(items) {
+    const valorInicial = 0;
+    let calcPrice = valorInicial;
+    items.forEach((item) => {
+      calcPrice += (item.price * item.initialQuantity);
+    });
+    return calcPrice;
+  }
+
   render() {
+    const cartItems = cartAPI.getCartItems();
+    console.log(cartItems);
     return (
       <div>
         <h3>Payment</h3>
+        <div>
+          <h4>Revise seus Produtos</h4>
+          { cartItems.map((item) => <ListItem key={ item.id } item={ item } />) }
+          <span>{`Total: R$${this.totalCartPrice(cartItems)}`}</span>
+        </div>
         <UserInfoForm onSubmit={ this.handleUserInfo } />
       </div>
     );
