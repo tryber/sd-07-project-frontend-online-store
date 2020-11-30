f76111aa5eba3387ee5572cc53da01342cc77209
Felipe Nascimento <flpnascto@gmail.com>
2020-11-30 14:34:30 -0300

    Adicionando rota para página de finalização da compra.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Payment from './pages/Payment';
 import Products from './pages/Products';
 import ShoppingCart from './pages/ShoppingCart';
 import ProductDetails from './pages/ProductDetails';
@@ -13,6 +14,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
+          <Route path="/payment" component={ Payment } />
           <Route path="/product-details" component={ ProductDetails } />
           <Route path="/shopping-cart" component={ ShoppingCart } />
           <Route exact path="/" component={ Products } />
