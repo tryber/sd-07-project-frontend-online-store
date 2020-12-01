7193b2eea1f6219d53e8a2efd04577f48abcef5a
Lenodeoliveira <lenodeoliveira@gmail.com>
2020-12-01 11:17:35 -0300

    Adicionando implementação do requisito 10

--- src/App.js
+++ src/App.js
@@ -8,13 +8,66 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.addCart = this.addCart.bind(this);
-    this.state = { cartItems: [] };
+    this.handleLocalStorage = this.handleLocalStorage.bind(this);
+    this.removeItemCart = this.removeItemCart.bind(this);
+    this.removeCart = this.removeCart.bind(this);
+    this.state = {
+      cartItems: [],
+    };
+  }
+
+  componentDidUpdate() {
+    this.handleLocalStorage();
+  }
+
+  handleLocalStorage() {
+    const { cartItems } = this.state;
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
   }
 
   addCart(product) {
-    this.setState(({ cartItems }) => ({
-      cartItems: [...cartItems, product],
+    this.setState((previousValue) => ({
+      cartItems: previousValue.cartItems.find(
+        (p) => p.id === product.id,
+      )
+        ? previousValue.cartItems.map((p) => {
+          if (p.id === product.id) {
+            return {
+              ...p,
+              countItems: p.countItems + 1,
+            };
+          }
+          return p;
+        }) : [...previousValue.cartItems, { ...product, countItems: 1 }],
     }));
+  }
+
+  removeItemCart(product) {
+    this.setState((previousValue) => ({
+      cartItems: previousValue.cartItems.some(
+        (p) => p.id === product.id,
+      )
+        ? previousValue.cartItems.map((p) => {
+          if (p.id === product.id) {
+            if (p.countItems) {
+              return {
+                ...p,
+                countItems: p.countItems - 1,
+              };
+            }
+          }
+          return p;
+        }) : [...previousValue.cartItems, { ...product, countItems: 1 }],
+    }));
+  }
+
+  removeCart(productRemove) {
+    this.setState((previousValue) => {
+      const listItems = [...previousValue.cartItems];
+      const indiceItem = listItems.findIndex((p) => p.id === productRemove.id);
+      listItems.splice(indiceItem, 1);
+      return { cartItems: listItems };
+    });
   }
 
   render() {
@@ -26,7 +79,14 @@ class App extends Component {
           <Switch>
             <Route
               path="/cart"
-              render={ (props) => <ShoppingCart { ...props } cartItems={ cartItems } /> }
+              render={ (props) => (
+                <ShoppingCart
+                  { ...props }
+                  addCart={ this.addCart }
+                  removeItemCart={ this.removeItemCart }
+                  removeCart={ this.removeCart }
+                  cartItems={ cartItems }
+                />) }
             />
             <Route
               path="/productdetails/:id"
