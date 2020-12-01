610f7b79bc3ff4a00c8076d78aab03b3a38fbba4
Daniel Silva Céspedes <dev.danielcespedes@gmail.com>
2020-12-01 19:41:05 -0300

    Requisito 9 com base em storage

---
+++ src/pages/DetalhesDoProduto.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import BotaoCarrinho from '../components/BotaoCarrinho';
+import * as FunctionsToCart from '../components/FunctionsToCart';
+
+class DetalhesDoProduto extends React.Component {
+  render() {
+    const jsonParseGetItem = JSON.parse(localStorage.getItem('productDetails'));
+    const {
+      title,
+      price,
+      condition,
+      thumbnail,
+      id,
+    } = jsonParseGetItem;
+    return (
+      <div>
+        <BotaoCarrinho />
+        <div>
+          <h2 data-testid="product-detail-name">{title}</h2>
+          <span>{price}</span>
+          <br />
+          <img src={ thumbnail } alt="imagem de um produto" />
+        </div>
+        <ul>
+          <li>
+            Condição do produto:
+            {condition}
+          </li>
+          <li>
+            Quantidade disponível:
+            {id}
+          </li>
+          <li>
+              Preço:
+              {price}
+          </li>
+        </ul>
+        <button
+          data-testid="product-detail-add-to-cart"
+          type="button"
+          onClick={ () => FunctionsToCart
+            .addToCart(title, price, thumbnail) }
+        >
+          Adicionar ao seu carrinho
+        </button>
+      </div>
+    );
+  }
+}
+
+export default DetalhesDoProduto;
