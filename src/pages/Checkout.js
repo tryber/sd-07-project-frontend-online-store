445f6de870587f417544d041456839c5b3fc371b
alexandrefaustino <alexandrefaculdade@gmail.com>
2020-12-02 22:52:13 -0300

    requisito 12v2

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -5,9 +5,9 @@ import BuyerInformation from '../components/BuyerInformation';
 
 class Checkout extends Component {
   render() {
-    const { location } = this.props;    
+    const { location } = this.props;
     const { state } = location;
-    const { productsInShoppingCart } = state;   
+    const { productsInShoppingCart } = state;
     return (
       <div>
         <ShoppingCardListBuy products={ productsInShoppingCart } />
