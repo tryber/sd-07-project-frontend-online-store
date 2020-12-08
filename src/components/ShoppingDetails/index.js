bea0f2c38ebf2d74909c8c8b8dcaed09fb2a59d1
Luiz Mariz <lfsmariz@gmail.com>
2020-12-08 09:46:11 -0300

    requirement 14

--- src/components/ShoppingDetails/index.js
+++ src/components/ShoppingDetails/index.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-import Wrapper from './style';
+import Wrapper from "./style";
 
 function ShoppingDetails({ item, modifyCart }) {
   const { title, price, id } = item;
@@ -13,46 +13,55 @@ function ShoppingDetails({ item, modifyCart }) {
   const { pathname: page } = window.location;
 
   const changeQuantity = ({ target: { value } }) => {
-    const stringCarrinho = localStorage.getItem('carrinho');
+    const stringCarrinho = localStorage.getItem("carrinho");
     const arrayCarrinho = JSON.parse(stringCarrinho);
 
-    const product = arrayCarrinho.find((itemCarrinho) => itemCarrinho.id === id);
+    const product = arrayCarrinho.find(
+      (itemCarrinho) => itemCarrinho.id === id
+    );
 
-    if (value === '+') {
-      product.quantity += 1;
-      product.total = product.quantity * product.price;
+    if (value === "+") {
+      if (product.quantity < product.available_quantity) {
+        product.quantity += 1;
+        product.total = product.quantity * product.price;
 
-      setQuantity(quantity + 1);
-      setTotal(product.quantity * product.price);
+        setQuantity(quantity + 1);
+        setTotal(product.quantity * product.price);
+      }
     } else if (product.quantity > 1) {
-      product.quantity -= 1;
-      product.total = product.quantity * product.price;
+      if (product.quantity < product.available_quantity) {
+        product.quantity -= 1;
+        product.total = product.quantity * product.price;
 
-      setQuantity(quantity - 1);
-      setTotal(product.quantity * product.price);
+        setQuantity(quantity - 1);
+        setTotal(product.quantity * product.price);
+      }
     }
 
-    localStorage.setItem('carrinho', JSON.stringify(arrayCarrinho));
+    localStorage.setItem("carrinho", JSON.stringify(arrayCarrinho));
   };
 
   const addToCart = () => {
-    const stringCarrinho = localStorage.getItem('carrinho');
+    const stringCarrinho = localStorage.getItem("carrinho");
     const arrayCarrinho = JSON.parse(stringCarrinho);
 
     if (!stringCarrinho) {
-      localStorage.setItem('carrinho', JSON.stringify([item]));
+      localStorage.setItem("carrinho", JSON.stringify([item]));
       modifyCart();
     } else {
-      const product = arrayCarrinho.find((itemCarrinho) => id === itemCarrinho.id);
+      const product = arrayCarrinho.find(
+        (itemCarrinho) => id === itemCarrinho.id
+      );
 
       if (product) {
-        product.quantity += 1;
+        if (product.quantity < product.available_quantity)
+          product.quantity += 1;
         product.total = product.quantity * product.price;
-        localStorage.setItem('carrinho', JSON.stringify(arrayCarrinho));
+        localStorage.setItem("carrinho", JSON.stringify(arrayCarrinho));
         modifyCart();
       } else {
         arrayCarrinho.push(item);
-        localStorage.setItem('carrinho', JSON.stringify(arrayCarrinho));
+        localStorage.setItem("carrinho", JSON.stringify(arrayCarrinho));
         modifyCart();
       }
     }
@@ -63,7 +72,7 @@ function ShoppingDetails({ item, modifyCart }) {
       value="+"
       type="button"
       data-testid="product-increase-quantity"
-      onClick={ changeQuantity }
+      onClick={changeQuantity}
     />
   );
   const decreaseButton = (
@@ -71,14 +80,14 @@ function ShoppingDetails({ item, modifyCart }) {
       value="-"
       type="button"
       data-testid="product-decrease-quantity"
-      onClick={ changeQuantity }
+      onClick={changeQuantity}
     />
   );
 
   const addToCartButton = (
     <button
       type="button"
-      onClick={ addToCart }
+      onClick={addToCart}
       data-testid="product-detail-add-to-cart"
     >
       Adicionar
@@ -89,21 +98,21 @@ function ShoppingDetails({ item, modifyCart }) {
     <Wrapper>
       <div data-testid="shopping-cart-product-name">{title}</div>
 
-      {page === '/cart' && decreaseButton}
+      {page === "/cart" && decreaseButton}
 
       <div data-testid="shopping-cart-product-quantity">{quantity}</div>
 
-      {page === '/cart' && increaseButton}
+      {page === "/cart" && increaseButton}
 
       <div data-testid="shopping-cart-product-price">{`R$ ${price}`}</div>
       <div>{`R$ ${total.toFixed(decimalPlates)}`}</div>
 
-      {page !== '/cart' && addToCartButton}
+      {page !== "/cart" && addToCartButton}
     </Wrapper>
   );
 }
 
-ShoppingDetails.defaultProps = { modifyCart: () => { } };
+ShoppingDetails.defaultProps = { modifyCart: () => {} };
 
 ShoppingDetails.propTypes = {
   item: PropTypes.shape({
