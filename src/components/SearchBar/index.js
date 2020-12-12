0419f9319e2485cb3a125ba0005cded4caa164ec
ygorvmdf <ygorvitor.fonseca@gmail.com>
2020-12-12 17:24:02 -0300

    estilizando header

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 
 class SearchBar extends Component {
@@ -23,14 +24,17 @@ class SearchBar extends Component {
             />
             <button
               id="input-text"
-              className="btn btn-info btn-xs"
+              className="btn btn-info btn-xs search-button"
               data-testid="query-button"
               type="button"
               onClick={handleSubmit}
             >
-              <i class="bi bi-search"></i>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z" />
+                <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z" />
+              </svg>
               Pesquisar
-          </button>
+            </button>
           </div>
         </div>
       </main>
