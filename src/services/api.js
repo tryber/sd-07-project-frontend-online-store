ed52403d10fd0e8600f0ae47e34d0c99fce55b37
Carol Bezerra <carol.arbe@gmail.com>
2020-12-21 09:25:13 -0300

    Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,19 @@
 export async function getCategories() {
-  // Implemente aqui
+  const requestAPI = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  return requestAPI.json();
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  if (categoryId && !query) {
+    const requestAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+    return requestAPI.json();
+  }
+  if (!categoryId && query) {
+    const requestAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${query}`);
+    return requestAPI.json();
+  }
+  if (categoryId && query) {
+    const requestAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
+    return requestAPI.json();
+  }
 }
