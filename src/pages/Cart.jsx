45abf0f93ba62f529d1575b6c0eb48d045821376
Carol Bezerra <carol.arbe@gmail.com>
2020-12-21 14:13:13 -0300

    Requisito 4

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Cart extends Component {
   render() {
     return (
       <div>
+        <Link to="/">Voltar</Link>
         <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
       </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,53 +1,49 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import { getCategories /* getProductsFromCategoryAndQuery */ } from '../services/api';
 
 class Home extends Component {
   constructor() {
     super();
     this.state = {
-      categories: ['hello'],
-      categoryId: '',
-      query: '',
-      result: '',
-      search: false,
-    }
+      categories: [],
+      // categoryId: '',
+      // query: '',
+      // result: '',
+      // search: false,
+    };
     this.upCategoryId = this.upCategoryId.bind(this);
   }
 
   componentDidMount() {
-    getCategories().then(() => this.setState({ categories: ['hello', 'hi'] }));
+    getCategories().then((categories) => this.setState({ categories }));
   }
 
-  upCategoryId(search) {
-    this.setState({ categoryId: search }, () => {
-      const { categoryId, query } = this.state;
-      getProductsFromCategoryAndQuery(categoryId, query)
-      .then(result => { this.setState({ result, search: true }) });
-    });
-  }
-
-
-
+  // upCategoryId(search) {
+  //   this.setState({ categoryId: search }, () => {
+  //     const { categoryId, query } = this.state;
+  //     getProductsFromCategoryAndQuery(categoryId, query)
+  //      .then(result => { this.setState({ result, search: true, }) });
+  //   });
+  // }
 
   render() {
     const { categories } = this.state;
-    console.log(categories);
     return (
       <div>
-        {categories.map(({ id, name }) => 
+        {categories.map(({ id, name }) => (
           <div key={ id }>
             <label htmlFor={ name }>
               <input
                 data-testid="category"
                 type="radio"
-                name={ name }
+                name="categories"
                 onChange={ () => this.upCategoryId(id) }
               />
               { name }
             </label>
           </div>
-        )}
+        ))}
         <input type="text" />
         <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
         <p data-testid="home-initial-message">
