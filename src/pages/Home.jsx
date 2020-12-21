662194b4e55be4601c6cc43d1d6f93c763e572dc
Carol Bezerra <carol.arbe@gmail.com>
2020-12-21 13:46:07 -0300

    Tentando requisito 4

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
 
 class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: ['hello'],
+      categoryId: '',
+      query: '',
+      result: '',
+      search: false,
+    }
+    this.upCategoryId = this.upCategoryId.bind(this);
+  }
+
+  componentDidMount() {
+    getCategories().then(() => this.setState({ categories: ['hello', 'hi'] }));
+  }
+
+  upCategoryId(search) {
+    this.setState({ categoryId: search }, () => {
+      const { categoryId, query } = this.state;
+      getProductsFromCategoryAndQuery(categoryId, query)
+      .then(result => { this.setState({ result, search: true }) });
+    });
+  }
+
+
+
+
   render() {
+    const { categories } = this.state;
+    console.log(categories);
     return (
       <div>
+        {categories.map(({ id, name }) => 
+          <div key={ id }>
+            <label htmlFor={ name }>
+              <input
+                data-testid="category"
+                type="radio"
+                name={ name }
+                onChange={ () => this.upCategoryId(id) }
+              />
+              { name }
+            </label>
+          </div>
+        )}
         <input type="text" />
         <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
         <p data-testid="home-initial-message">
--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,25 @@
 export async function getCategories() {
   const requestAPI = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
-  return requestAPI.json();
+  const requestJson = await requestAPI.json();
+  return requestJson;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  if (categoryId && !query) {
-    const requestAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
-    return requestAPI.json();
-  }
-  if (!categoryId && query) {
-    const requestAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${query}`);
-    return requestAPI.json();
-  }
   if (categoryId && query) {
     const requestAPI = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`);
-    return requestAPI.json();
+    const requestJson = await requestAPI.json();
+    return requestJson;
+  }
+
+  if (categoryId && !query) {
+    const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`);
+    const requestJson = await request.json();
+    return requestJson;
+  }
+
+  if (!categoryId && query) {
+    const request = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+    const requestJson = await request.json();
+    return requestJson;
   }
 }
